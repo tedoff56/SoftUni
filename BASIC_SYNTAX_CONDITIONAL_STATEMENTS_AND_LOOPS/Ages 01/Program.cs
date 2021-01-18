8a5e9b57b084e0cf09fe2387f59728e1379921ae
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-01-18 18:52:33 +0200

    1

---
+++ BASIC_SYNTAX_CONDITIONAL_STATEMENTS_AND_LOOPS/Ages 01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ages_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ages = int.Parse(Console.ReadLine());
+            string person = "";
+            if (ages >= 0 && ages <= 2)
+                person = "baby";
+            else if (ages >= 3 && ages <= 13)
+                person = "child";
+            else if (ages >= 14 && ages <= 19)
+                person = "teenager";
+            else if (ages >= 20 && ages <= 65)
+                person = "adult";
+            else if (ages >= 66)
+                person = "elder";
+            Console.WriteLine(person);
+        }
+    }
+}
