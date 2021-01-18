00a8adf2066427e53e27d7a5789ee288bc943f8b
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-01-18 21:55:28 +0200

    up

---
+++ BASIC_SYNTAX_CONDITIONAL_STATEMENTS_AND_LOOPS/Strong_Number 06/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Strong_Number_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            char[] nums = number.ToCharArray();
+            int totalSum = 0;
+            
+            for (int i = 0; i < number.Length; i++)
+            {
+                int currentNum = nums[i] - '0';
+                
+                int fact = 1;
+                for (int j = 1; j <= currentNum; j++)
+                {
+                    fact *= j;
+                }
+                
+                totalSum += fact;
+            }
+
+            if (number == totalSum.ToString())
+                Console.WriteLine("yes");
+            else
+                Console.WriteLine("no");
+
+        }
+    }
+}
