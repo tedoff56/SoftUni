f2dc05d330866800c64ad863a516b830766cd1a3
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-01-26 22:41:44 +0200

    up

---
+++ DATA_TYPES_AND_VARIABLES/Spice_Must_Flow 09/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Spice_Must_Flow_09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startingYield = int.Parse(Console.ReadLine());
+            int days= 0;
+            long totalAmount = 0;
+
+            if (startingYield >= 100)
+            {
+                while (startingYield >= 100)
+                {
+                    days++;
+                    totalAmount += startingYield - 26;
+                    startingYield -= 10;
+                }
+
+                Console.WriteLine($"{days}\n{totalAmount - 26}");
+            }
+            else
+            {
+                Console.WriteLine($"0\n0");
+            }
+        }
+    }
+}
