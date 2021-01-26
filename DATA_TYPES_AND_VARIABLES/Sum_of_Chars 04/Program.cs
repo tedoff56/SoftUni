766d35bab4b566c7bcd950b38b73034f3a1cd936
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-01-26 21:24:06 +0200

    update

---
+++ DATA_TYPES_AND_VARIABLES/Sum_of_Chars 04/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Sum_of_Chars_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int n = int.Parse(Console.ReadLine());
+            
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                char letter = Char.Parse(Console.ReadLine());
+                sum += letter;
+            }
+            Console.WriteLine($"The sum equals: {sum}");
+        }
+    }
+}
