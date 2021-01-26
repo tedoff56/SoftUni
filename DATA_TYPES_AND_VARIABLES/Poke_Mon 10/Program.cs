3274e1444ed761e5f3aba2c4a2930a216af836a6
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-01-26 23:00:13 +0200

    update

---
+++ DATA_TYPES_AND_VARIABLES/Poke_Mon 10/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Poke_Mon_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pokePowerN = int.Parse(Console.ReadLine());
+            int distanceM = int.Parse(Console.ReadLine());
+            int exhaustionFactorY = int.Parse(Console.ReadLine());
+            int totalTargets = 0;
+            int originalN = pokePowerN;
+            
+            while (!(pokePowerN < distanceM))
+            {
+
+                pokePowerN -= distanceM;
+                totalTargets++;
+
+                if (pokePowerN == originalN * 0.5 && exhaustionFactorY != 0)
+                {
+                    pokePowerN /= exhaustionFactorY;
+                }
+            }
+
+            Console.WriteLine($"{pokePowerN}\n{totalTargets}");
+        }
+    }
+}
