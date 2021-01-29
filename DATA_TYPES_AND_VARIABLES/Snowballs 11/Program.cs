ee94fd305ab295fa02cac7efc6b13323ba09e9cd
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-01-29 22:52:13 +0200

    up

---
+++ DATA_TYPES_AND_VARIABLES/Snowballs 11/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Snowballs_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int totalSnowballsN = int.Parse(Console.ReadLine());
+            
+            int highestSnowballSnow = 0;
+            int highestSnowballTime = 0;
+            int highestSnowballQuality = 0;
+            double highestSnowballValue = int.MinValue;
+
+            for (int i = 0; i < totalSnowballsN; i++)
+            {
+                int snowballSnow = int.Parse(Console.ReadLine());
+                int snowballTime = int.Parse(Console.ReadLine());
+                int snowballQuality = int.Parse(Console.ReadLine());
+                double snowballValue = Math.Pow((double)snowballSnow / snowballTime, snowballQuality);
+                
+                if (snowballValue > highestSnowballValue)
+                {
+                    highestSnowballSnow = snowballSnow;
+                    highestSnowballTime = snowballTime;
+                    highestSnowballQuality= snowballQuality;
+                    highestSnowballValue = snowballValue;
+                }
+            }
+            Console.WriteLine($"{highestSnowballSnow} : {highestSnowballTime} = {highestSnowballValue} ({highestSnowballQuality})");
+            
+        }
+    }
+}
