3630b869ccbf04ffa07d3da13004575b4cb917b4
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-01-30 00:18:54 +0200

    update

---
+++ 3 ARRAYS/Train 01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Train_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            int sum = 0;
+            
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                sum += arr[i];
+                Console.Write($"{arr[i]} ");
+            }
+            Console.WriteLine("");
+            Console.WriteLine($"{sum}");
+            
+
+        }
+    }
+}
