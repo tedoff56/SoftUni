9e92e0bc664589fb5e4f2004d8c36b9c51386cf1
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-01-30 15:45:34 +0200

    update

---
+++ 3 ARRAYS/Common_Elements 02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Common_Elements_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string second = Console.ReadLine();
+            string first = Console.ReadLine();
+            
+            string[] firstArr = first.Split();
+            string[] secondArr = second.Split();
+            
+            
+                for (int i = 0; i < firstArr.Length; i++)
+                {
+                    for (int j = 0; j < secondArr.Length; j++)
+                    {
+                        if(firstArr[i].Equals(secondArr[j]))
+                            Console.Write($"{secondArr[j]} ");
+                    }
+                }
+        }
+    }
+}
