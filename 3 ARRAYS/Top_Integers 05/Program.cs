fdc35e0f1059646f25af603d2681d591f7df5d74
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-01-31 19:53:45 +0200

    up

---
+++ 3 ARRAYS/Top_Integers 05/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Top_Integers_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                bool isTopNumber = true;
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] <= numbers[j])
+                    {
+                        isTopNumber = false;
+                        break;
+                    }
+                }
+                if(isTopNumber)
+                    Console.Write($"{numbers[i]} ");
+            }
+        }
+    }
+}
