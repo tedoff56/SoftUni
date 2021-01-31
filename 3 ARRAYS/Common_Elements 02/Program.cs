1459be1299d24f78ad9b1f7a222e69d49446d15d
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-01-31 10:43:50 +0200

    up

--- 3 ARRAYS/Common_Elements 02/Program.cs
+++ 3 ARRAYS/Common_Elements 02/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
+using System.Linq;
 
 namespace Common_Elements_02
 {
---
+++ 3 ARRAYS/Zig-Zag_Arrays 03/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Zig_Zag_Arrays_03
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            
+            int n = int.Parse(Console.ReadLine());
+            int[] firstArr = new int[n];
+            int[] secondArr = new int[n];
+
+            for (var i = 0; i < n; i++)
+            { 
+                string numbersRead = Console.ReadLine();
+                string[] numbers = numbersRead.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                
+                        if(i % 2 == 0)
+                        {
+                            firstArr[i] = int.Parse(numbers[0]);
+                            secondArr[i] = int.Parse(numbers[1]);
+                        }
+                        else
+                        {
+                            firstArr[i] = int.Parse(numbers[1]);
+                            secondArr[i] = int.Parse(numbers[0]);
+                        }
+                        
+            }
+            
+            for (var i = 0; i < firstArr.Length; i++)
+            {
+                Console.Write($"{firstArr[i]} ");
+            }
+                Console.WriteLine("");
+            for (var i = 0; i < firstArr.Length; i++)
+            {
+                Console.Write($"{secondArr[i]} ");
+            }
+            
+        }
+    }
+}
