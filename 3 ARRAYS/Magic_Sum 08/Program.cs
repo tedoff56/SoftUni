2553fb25bbec378d3e01f68f6118ce3920d9c08e
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-02-03 18:14:57 +0200

    up

---
+++ 3 ARRAYS/Magic_Sum 08/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace Magic_Sum_08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = i; j < numbers.Length; j++)
+                {
+                    if (numbers[i] + numbers[j] == n && i != j)
+                    {
+                        Console.Write($"{numbers[i]} {numbers[j]}");
+                        Console.WriteLine("");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 3 ARRAYS/Max _Sequence_of_Equal_Elements 07/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace Max__Sequence_of_Equal_Elements_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            int longestSequence = int.MinValue;
+            int longestSequenceNum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+
+            {
+                int sequenceCnt = 0;
+                int searchingFor = numbers[i];
+                
+                for (int j = i; j < numbers.Length; j++)
+                {
+                    if (searchingFor != numbers[j])
+                    {
+                        break;
+                    }
+
+                    sequenceCnt++;
+                }
+
+                if (sequenceCnt > longestSequence)
+                {
+                    longestSequence = sequenceCnt;
+                    longestSequenceNum = searchingFor;
+                }
+            }
+
+            for (int i = 0; i < longestSequence; i++)
+            {
+                Console.Write($"{longestSequenceNum} ");
+            }
+        }
+    }
+}
