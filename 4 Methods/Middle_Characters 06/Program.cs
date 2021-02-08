fde83a25ac649bacb97ec423b5cb1d711ba9b93b
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-02-08 20:58:33 +0200

    up

---
+++ 4 Methods/Middle_Characters 06/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Middle_Characters_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            MiddleChar(str);
+        }
+
+        static void MiddleChar(string input)
+        {
+            if (input.Length % 2 == 0)
+            {
+                Console.Write(input[(input.Length/2) - 1]);
+                Console.Write(input[input.Length/2]);
+            }
+            else
+            {
+                Console.WriteLine(input[(input.Length/2)]);
+            }
+        }
+    }
+}
