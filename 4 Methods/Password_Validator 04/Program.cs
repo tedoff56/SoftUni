0f9ca2f0f47bd95da90c03c74a2e133a47c9a38e
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-02-08 20:33:20 +0200

    up

--- 4 Methods/Password_Validator 04/Program.cs
+++ 4 Methods/Password_Validator 04/Program.cs
@@ -6,7 +6,74 @@ namespace Password_Validator_04
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            PasswordValidator();
+        }
+
+        static void PasswordValidator()
+        {
+            string password = Console.ReadLine();
+            
+            bool totalSizeValid = password.Length >= 6 && password.Length <= 10;
+            bool digAndLetValid = HasOnlyDigitsAndLeters(password);
+            bool atleastTwoDigits = AtleastTwoDigits(password);
+
+            bool passwordIsValid = totalSizeValid && digAndLetValid && atleastTwoDigits;
+
+            if (passwordIsValid)
+            {
+                Console.WriteLine("Password is valid");
+            }
+            if(!totalSizeValid)
+                Console.WriteLine("Password must be between 6 and 10 characters");
+            if(!digAndLetValid)
+                Console.WriteLine("Password must consist only of letters and digits");
+            if(!atleastTwoDigits)
+                Console.WriteLine("Password must have at least 2 digits");
+            
+
+        }
+
+        static bool HasOnlyDigitsAndLeters(string password)
+        {
+            int cnt = 0;
+            bool hasOnlyDigAndLet = false;
+            for (int i = 0; i < password.Length; i++)
+            {
+                if ((char) password[i] >= 48 && (char) password[i] <= 57 || 
+                    (char) password[i] >= 65 && (char) password[i] <= 90 || 
+                    (char) password[i] >= 97 && (char) password[i] <= 122)
+                {
+                    cnt++;
+                }
+                
+            }
+
+            if (cnt == password.Length)
+            {
+                hasOnlyDigAndLet = true;
+            }
+
+            return hasOnlyDigAndLet;
+        }
+
+        static bool AtleastTwoDigits(string password)
+        {
+            bool hasAtleastTwoDigits = false;
+            int totalDigits = 0;
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (password[i] >= 48 && password[i] <= 57)
+                {
+                    totalDigits++;
+                }
+            }
+
+            if (totalDigits >= 2)
+            {
+                hasAtleastTwoDigits = true;
+            }
+
+            return hasAtleastTwoDigits;
         }
     }
 }
