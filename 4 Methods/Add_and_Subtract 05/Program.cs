9622633ed795a8314ba6c1bfa8f133f30250d230
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-02-08 20:50:58 +0200

    up

---
+++ 4 Methods/Add_and_Subtract 05/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Add_and_Subtract_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Subtract(Sum(firstNumber, secondNumber), thirdNumber));
+
+        }
+
+        static int Sum(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+
+        static int Subtract(int n1, int n2)
+        {
+            return n1 - n2 ;
+        }
+    }
+}
