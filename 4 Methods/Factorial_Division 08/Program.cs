5ca1a2b17d2fc33f3a856c5849e85ee14944c288
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-02-10 22:27:42 +0200

    up

--- 4 Methods/Factorial_Division 08/Program.cs
+++ 4 Methods/Factorial_Division 08/Program.cs
@@ -6,27 +6,28 @@ namespace Factorial_Division_08
     {
         static void Main(string[] args)
         {
-            int firstNumber = int.Parse(Console.ReadLine());
-            int secondNumber = int.Parse(Console.ReadLine());
-            
-            Console.WriteLine($"{FactorialDivision(firstNumber, secondNumber):F2}");
-                
-        }
 
-        static double FactorialDivision(int firstNumber, int secondNubmer)
-        {
-            return (double)Factorial(firstNumber) / Factorial(secondNubmer);
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+            double division = (double)Factorial(number1) / Factorial(number2);
+            
+            Console.WriteLine($"{division:F2}");
+
+
         }
 
         static int Factorial(int number)
         {
+            
             int factorial = 1;
             for (int i = 1; i <= number; i++)
-            {
-                factorial *= i;
-            }
-
+                {
+                    factorial *= i;
+                }
+                
             return factorial;
+
         }
+        
     }
 }
---
+++ 4 Methods/Top_Number 10/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Top_Number_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            bool isTopNumber = false;
+            
+            for (int i = 0; i < number; i++)
+            {
+                isTopNumber = DigitsSumDivByEight(i.ToString()) && HasOddDigit(i.ToString());
+                if (isTopNumber)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            
+        }
+
+        static bool DigitsSumDivByEight(string num)
+        {
+            int sum = 0;
+            
+            for (int i = 0; i < num.Length; i++)
+            {
+                sum += num[i];
+            }
+
+            if (sum % 8 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static bool HasOddDigit(string num)
+        {
+            
+            for (int i = 0; i < num.Length; i++)
+            {
+                if (num[i] % 2 != 0)
+                {
+                    return true;
+                }
+            }
+            
+            return false;
+        }
+        
+    }
+}
