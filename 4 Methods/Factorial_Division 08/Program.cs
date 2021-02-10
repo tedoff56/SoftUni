a140a874278fe8f8fa41bba408235d0b8178dcd8
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-02-10 22:32:57 +0200

    up

--- 4 Methods/Factorial_Division 08/Program.cs
+++ 4 Methods/Factorial_Division 08/Program.cs
@@ -7,19 +7,19 @@ namespace Factorial_Division_08
         static void Main(string[] args)
         {
 
-            int number1 = int.Parse(Console.ReadLine());
-            int number2 = int.Parse(Console.ReadLine());
-            double division = (double)Factorial(number1) / Factorial(number2);
+            double number1 = int.Parse(Console.ReadLine());
+            double number2 = int.Parse(Console.ReadLine());
+            double division = Factorial(number1) / Factorial(number2);
             
             Console.WriteLine($"{division:F2}");
 
 
         }
 
-        static int Factorial(int number)
+        static double Factorial(double number)
         {
             
-            int factorial = 1;
+            double factorial = 1;
             for (int i = 1; i <= number; i++)
                 {
                     factorial *= i;
