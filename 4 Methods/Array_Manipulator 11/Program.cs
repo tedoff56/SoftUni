b3fd424894f1145ffe89925ef680057414f1e2b5
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-02-10 22:54:08 +0200

    up

---
+++ 4 Methods/Array_Manipulator 11/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Array_Manipulator_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbersArr = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+        }
+
+        static void exchange(int[] arr)
+        {
+            int index = int.Parse(Console.ReadLine());
+
+        }
+        
+    }
+}
