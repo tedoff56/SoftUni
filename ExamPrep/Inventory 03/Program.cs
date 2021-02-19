ab344406966af70d8e11d5a3e5160112c045e4ab
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-02-19 23:11:33 +0200

    up

---
+++ ExamPrep/Inventory 03/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Inventory_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> items = Console.ReadLine()
+                .Split(",", StringSplitOptions.TrimEntries)
+                .ToList();
+
+            while (true)
+            {
+                string line = Console.ReadLine();
+                if (line == "Craft!")
+                {
+                    break;
+                }
+
+                string[] commands = line.Split("-", StringSplitOptions.TrimEntries);
+
+                string action = commands[0];
+                string item = commands[1];
+
+                if (action == "Collect")
+                {
+                    if (!items.Contains(item))
+                    {
+                        items.Add(item);
+                    }
+                }
+                else if (action == "Drop")
+                {
+                    items.Remove(item);
+                }
+                else if (action == "Combine Items")
+                {
+                    string[] parameters = item.Split(":", StringSplitOptions.TrimEntries).ToArray();
+
+                    string oldItem = parameters[0];
+                    string newItem = parameters[1];
+                    
+                    if (items.Contains(oldItem))
+                    {
+                        items.Insert(items.IndexOf(oldItem) + 1, newItem);
+                    }
+                }
+                else if (action == "Renew")
+                {
+                    if (items.Contains(item))
+                    {
+                        items.Add(item);
+                        items.Remove(item);
+                    }
+                }
+
+            }
+
+            Console.WriteLine(string.Join(", ", items));
+
+        }
+    }
+}
---
+++ ExamPrep/MuOnline_02/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+
+namespace MuOnline_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int initialHealth = 100;
+            int initialBitcoins = 0;
+            
+            int currentHealth = initialHealth;
+            int currentBitcoins = initialBitcoins;
+
+            string line = Console.ReadLine();
+
+            string[] rooms = line
+                .Split("|", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            
+            for (int i = 0; i < rooms.Length; i++)
+            {
+                string[] currentRoom = rooms[i].Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string command = currentRoom[0];
+                int amount = int.Parse(currentRoom[1]);
+                int bestRoom = i + 1;
+
+
+                if (command == "potion")
+                {
+                    
+                    if (currentHealth + amount <= initialHealth)
+                    {
+                        currentHealth += amount;
+                        Console.WriteLine($"You healed for {amount} hp.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"You healed for {initialHealth - currentHealth} hp.");
+                        currentHealth = 100;
+
+                    }
+
+                    Console.WriteLine($"Current health: {currentHealth} hp.");
+                    
+                }
+                else if (command == "chest")
+                {
+                    currentBitcoins += amount;
+                    Console.WriteLine($"You found {amount} bitcoins.");
+                }
+                else
+                {
+                    string monster = command;
+                    currentHealth -= amount;
+
+                    if (currentHealth > 0)
+                    {
+                        Console.WriteLine($"You slayed {monster}.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"You died! Killed by {monster}.");
+                        Console.WriteLine($"Best room: {bestRoom}");
+                        break;
+                    }
+                }
+            }
+            if(currentHealth > 0)
+            {
+                Console.WriteLine($"You've made it!", "Bitcoins: {bitcoins}", $"Health: {currentHealth}");
+                Console.WriteLine($"Bitcoins: {currentBitcoins}");
+                Console.WriteLine($"Health: {currentHealth}");
+            }
+
+        }
+    }
+}
