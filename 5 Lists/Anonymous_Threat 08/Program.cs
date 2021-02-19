f09468400ffca99e5a27ac8048e2563cc6704f9f
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-02-19 22:36:37 +0200

    up

---
+++ 5 Lists/Anonymous_Threat 08/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Anonymous_Threat_08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ExamPrep/Bonus_Scoring_System_01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Bonus_Scoring_System_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalStudents = int.Parse(Console.ReadLine());
+            int lecturesInTheCourse = int.Parse(Console.ReadLine());
+            int initialBonus = int.Parse(Console.ReadLine());
+            
+            decimal maxBonusPoints = 0;
+            int bestStudentAttendances = int.MinValue;
+            
+            if (totalStudents > 0 && totalStudents <= 50 
+            && lecturesInTheCourse > 0 && lecturesInTheCourse <= 50
+            && initialBonus >= 0 && initialBonus <= 100)
+            {
+
+                for (int i = 0; i < totalStudents; i++)
+                {
+                    int studentAttendances = int.Parse(Console.ReadLine());
+                    decimal totalBonus = (decimal)studentAttendances / lecturesInTheCourse * (5 + initialBonus);
+                    if (studentAttendances > bestStudentAttendances)
+                    {
+                        maxBonusPoints = totalBonus;
+                        bestStudentAttendances = studentAttendances;
+                    }
+                }
+                
+                Console.WriteLine($"Max Bonus: {Math.Round(maxBonusPoints, MidpointRounding.ToPositiveInfinity):F0}.");
+                Console.WriteLine($"The student has attended {bestStudentAttendances} lectures.");
+
+            }
+        }
+    }
+}
