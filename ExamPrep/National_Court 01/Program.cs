b6992e479bb39ec5cccc7ec5e4a78f5ac8e90310
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-02-20 22:09:13 +0200

    up

---
+++ ExamPrep/National_Court 01/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstEmployee = int.Parse(Console.ReadLine());
+            var secondEmployee = int.Parse(Console.ReadLine());
+            var thirdEmployee = int.Parse(Console.ReadLine());
+            var totalPeople = int.Parse(Console.ReadLine());
+            
+            var helpPerHour = firstEmployee + secondEmployee + thirdEmployee;
+
+
+            var time = 0;
+            
+            // var time = Math.Round((double)totalPeople / helpPerHour, MidpointRounding.ToPositiveInfinity);
+            // if (time / 3 >= 0)
+            // {
+            //     time += Math.Round(time / 3, MidpointRounding.ToNegativeInfinity);
+            // }
+            
+            while (totalPeople > 0)
+            {
+                time++;
+                if (time % 4 == 0)
+                {
+                    continue;
+                }
+                
+                totalPeople -= helpPerHour;
+            }
+
+            Console.WriteLine($"Time needed: {time}h.");
+        }
+    }
+}
---
+++ ExamPrep/Shopping_List 02/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Shopping_List_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> shoppingList = Console.ReadLine()
+                .Split("!", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            while (true)
+            {
+                string line = Console.ReadLine();
+
+                string[] commands = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string command = commands[0];
+                if (line == "Go Shopping!")
+                {
+                    break;
+                }
+                string item = commands[1];
+
+                if (command == "Urgent")
+                {
+                    if (!shoppingList.Contains(item))
+                    {
+                        shoppingList.Insert(0, item);
+                    }
+                }
+                else if (command == "Unnecessary")
+                {
+                    if (shoppingList.Contains(item))
+                    {
+                        shoppingList.Remove(item);
+                    }
+                }
+                else if (command == "Correct")
+                {
+                    string oldItem = item;
+                    string newItem = commands[2];
+                    if (shoppingList.Contains(oldItem))
+                    {
+                        shoppingList.Insert(shoppingList.IndexOf(oldItem), newItem);
+                        shoppingList.Remove(oldItem);
+                    }
+                }
+                else if (command == "Rearrage")
+                {
+                    if (shoppingList.Contains(item))
+                    {
+                        shoppingList.Add(item);
+                        shoppingList.Remove(item);
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", shoppingList));
+        }
+    }
+}
