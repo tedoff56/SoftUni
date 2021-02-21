bb9bdb79f261d6bbc3b56dc662ad70e03d741ae9
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-02-21 20:01:15 +0200

    up

---
+++ 6 Objects and Classes/Advertisement_Message 01/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Advertisement_Message_01
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] phrases = new string[]
+            {
+                "Excellent product.", 
+                "Such a great product.", 
+                "I always use that product.",
+                "Best product of its category.", 
+                "Exceptional product.", 
+                "I can’t live without this product."
+            };
+            string[] events = new string[]
+            {
+                "Now I feel good.", 
+                "I have succeeded with this product.", 
+                "Makes miracles. I am happy of the results!",
+                "I cannot believe but now I feel awesome.", 
+                "Try it yourself, I am very satisfied.", 
+                "I feel great!"
+            };
+            string[] authors = new string[] {
+                "Diana", "Petya", "Stella", "Elena", 
+                "Katya", "Iva", "Annie", "Eva"
+            };
+            string[] cities = new string[]
+            {
+                "Burgas", "Sofia", "Plovdiv", "Varna", "Ruse"
+            };
+
+            int n = int.Parse(Console.ReadLine());
+            Random rnd = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"{phrases[rnd.Next(phrases.Length - 1)]} " +
+                                  $"{events[rnd.Next(events.Length - 1)]} " +
+                                  $"{authors[rnd.Next(authors.Length - 1)]} – " +
+                                  $"{cities[rnd.Next(cities.Length - 1)]}");
+            }
+        }
+    }
+}
