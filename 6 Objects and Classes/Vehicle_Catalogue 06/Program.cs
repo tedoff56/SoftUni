a3e0d911f9c71d3cd7b8b78998b6df2b85c6a648
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-03-05 20:59:33 +0200

    up

---
+++ 6 Objects and Classes/Vehicle_Catalogue 06/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Vehicle_Catalogue_06
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            
+        }
+    }
+}
---
+++ 7 ASSOCIATIVE ARRAYS/Count_Chars_in_a_String 01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Count_Chars_in_a_String_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, int> symbols = new Dictionary<char, int>();
+
+            string text = Console.ReadLine();
+            for (int i = 0; i < text.Length; i++)
+            {
+                char currentSymbol = text[i];
+                if(symbols.ContainsKey(currentSymbol))
+                {
+                    symbols[currentSymbol]++;
+                }
+                else
+                {
+                    symbols.Add(currentSymbol, 1);
+                }
+            }
+
+            foreach (var symbol in symbols)
+            {
+                if(symbol.Key != ' ')
+                {
+                    Console.WriteLine($"{symbol.Key} -> {symbol.Value}");
+                }
+            }
+
+        }
+    }
+}
