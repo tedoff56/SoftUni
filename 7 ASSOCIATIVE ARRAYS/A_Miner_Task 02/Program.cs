c043fd25d660297a23237743d34fccbf27de9aa8
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-03-05 21:09:20 +0200

    up

---
+++ 7 ASSOCIATIVE ARRAYS/A_Miner_Task 02/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace A_Miner_Task_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> resources = new Dictionary<string, int>();
+
+            while (true)
+            {
+                string resource = Console.ReadLine();
+                if (resource == "stop")
+                {
+                    break;
+                }
+
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (resources.ContainsKey(resource))
+                {
+                    resources[resource] += quantity;
+                }
+                else
+                {
+                    resources.Add(resource, quantity);
+                }
+            }
+
+            foreach (var resource in resources)
+            {
+                Console.WriteLine($"{resource.Key} -> {resource.Value}");
+            }
+        }
+    }
+}
