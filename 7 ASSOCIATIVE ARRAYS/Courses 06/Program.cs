97893ee9a3042810e595c669833a4e7474ed96e9
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-03-08 21:40:15 +0200

    up

---
+++ 7 ASSOCIATIVE ARRAYS/Courses 06/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Courses_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<string, List<string>> courses = new Dictionary<string, List<string>>();
+
+            while (true)
+            {
+                string[] tokens = Console.ReadLine()
+                    .Split(" : ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (tokens[0] == "end")
+                {
+                    break;
+                }
+
+                string courseName = tokens[0];
+                string studentName = tokens[1];
+
+                if (!courses.ContainsKey(courseName))
+                {
+                    courses.Add(courseName, new List<string>(){studentName});
+                }
+                else
+                {
+                    courses[courseName].Add(studentName);
+                }
+                
+            }
+
+            foreach (var course in courses.OrderByDescending(n => n.Value.Count))
+            {
+                Console.WriteLine($"{course.Key}: {course.Value.Count}");
+                foreach (var name in course.Value.OrderBy(n => n))
+                {
+                    Console.WriteLine($"-- {name}");
+                }
+            }
+        }
+    }
+}
