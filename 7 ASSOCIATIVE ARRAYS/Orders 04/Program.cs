41e3022c2c61864a1c413e5de7ca6c3671288ce4
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-03-08 21:20:44 +0200

    up

---
+++ 7 ASSOCIATIVE ARRAYS/Orders 04/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Orders_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<double>> products = new Dictionary<string, List<double>>();
+
+            while (true)
+            {
+                string[] tokens = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                if (tokens[0] == "buy")
+                {
+                    break;
+                }
+
+                string product = tokens[0];
+                double price = double.Parse(tokens[1]);
+                double quantity = int.Parse(tokens[2]);
+
+                if (products.ContainsKey(product))
+                {
+                    products[product][1] += quantity;
+                    products[product][0] = price;
+                }
+                else
+                {
+                    products.Add(product, new List<double>(){price, quantity});
+                }
+                
+            }
+
+            foreach (var product in products)
+            {
+                double totalPrice = product.Value[0] * product.Value[1];
+                Console.WriteLine($"{product.Key} -> {totalPrice:F2}");
+            }
+            
+        }
+    }
+}
---
+++ 7 ASSOCIATIVE ARRAYS/SoftUni_Parking 05/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SoftUni_Parking_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, string> usersDataBase = new Dictionary<string, string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] tokens = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                
+                string command = tokens[0];
+                string name = tokens[1];
+
+                if (command == "register")
+                {
+                    string licensePlateNumber = tokens[2];
+
+                    if (usersDataBase.ContainsKey(name))
+                    {
+                        Console.WriteLine($"ERROR: already registered with plate number {licensePlateNumber}");
+                    }
+                    else
+                    {
+                        usersDataBase.Add(name, licensePlateNumber);
+                        Console.WriteLine($"{name} registered {licensePlateNumber} successfully");
+                    }
+
+                }
+                else if (command == "unregister")
+                {
+                    if (!usersDataBase.ContainsKey((name)))
+                    {
+                        Console.WriteLine($"ERROR: user {name} not found");
+                    }
+                    else
+                    {
+                        usersDataBase.Remove(name);
+                        Console.WriteLine($"{name} unregistered successfully");
+                    }
+                    
+                }
+            }
+
+            foreach (var user in usersDataBase)
+            {
+                Console.WriteLine($"{user.Key} => {user.Value}");
+            }
+        }
+    }
+}
