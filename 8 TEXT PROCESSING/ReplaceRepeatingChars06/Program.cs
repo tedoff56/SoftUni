686468aacb944932caaa2d49c4a54a35a418a5e0
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-03-14 17:32:22 +0200

    up

---
+++ 8 TEXT PROCESSING/ReplaceRepeatingChars06/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ReplaceRepeatingChars06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder text = new StringBuilder(Console.ReadLine());
+            StringBuilder result = new StringBuilder();
+
+            char lastChar = ' ';
+            for (int i = 0; i < text.Length; i++)
+            {
+                char currentChar = text[i];
+                if (currentChar != lastChar)
+                {
+                    result.Append(currentChar);
+                    lastChar = currentChar;
+                }
+            }
+            
+            Console.WriteLine(result);
+        }
+    }
+}
