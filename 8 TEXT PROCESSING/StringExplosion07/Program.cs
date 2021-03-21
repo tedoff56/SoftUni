64589f41a0531d42dae6e417ab81100dc5635c2e
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-03-21 14:12:06 +0200

    regex

---
+++ 8 TEXT PROCESSING/StringExplosion07/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace StringExplosion07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder input = new StringBuilder(Console.ReadLine());
+            StringBuilder result = new StringBuilder();
+            result.Append(input);
+            for (int i = 0; i < input.Length; i++)
+            {
+                char currentChar = input[i];
+
+                if (currentChar != '>')
+                {
+                    continue;;
+                }
+                int strength = input[i + 1] - '0';
+
+                
+
+
+            }
+        }
+    }
+}
---
+++ 9 REGULAR EXPRESSIONS/Furniture 01/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Furniture_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> boughtFurniture = new List<string>();
+            double totalSum = 0;
+
+            string pattern = @"(?<name>\w+)<<(?<price>[0-9.]+)!(?<quantity>[0-9]+)";
+            Regex regex = new Regex(pattern);
+            
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "Purchase")
+                {
+                    break;
+                }
+
+                var match = regex.Match(input);
+                if (!regex.IsMatch(input))
+                {
+                    continue;
+                }
+                
+                string name = match.Groups["name"].Value;
+                double price = double.Parse(match.Groups["price"].Value);
+                int quantity = int.Parse(match.Groups["quantity"].Value);
+
+                boughtFurniture.Add(name);
+                totalSum += price * quantity;
+
+            }
+
+            Console.WriteLine("Bought furniture:");
+            foreach (var furniture in boughtFurniture)
+            {
+                Console.WriteLine($"{furniture}");
+            }
+
+            Console.WriteLine($"Total money spend: {totalSum:F2}");
+        }
+    }
+}
