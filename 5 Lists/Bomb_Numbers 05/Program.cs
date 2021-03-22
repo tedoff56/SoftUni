f06422beb5b4d52ea0bbd44185d14aca434e0185
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-03-22 21:05:30 +0200

    bomb numbers

--- 5 Lists/Bomb_Numbers 05/Program.cs
+++ 5 Lists/Bomb_Numbers 05/Program.cs
@@ -8,49 +8,44 @@ namespace Bomb_Numbers_05
     {
         static void Main(string[] args)
         {
-            List<int> numbersList = Console.ReadLine()
-                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            List<int> numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
                 .Select(int.Parse)
                 .ToList();
 
-            int[] line = Console.ReadLine()
-                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            int[] bombData = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
                 .Select(int.Parse)
                 .ToArray();
-            
-            int bombNumber = line[0];
-            int bombPower = line[1];
-            
-            DetonateBomb(numbersList, bombNumber, bombPower);
-            
-            Console.WriteLine($"{numbersList.Sum()}");
-        }
 
-        static void DetonateBomb(List<int> numbers, int bombNumber, int bombPower)
-        {
-            while (true)
+            int bombNumber = bombData[0];
+            int bombPower = bombData[1];
+            
+            while (numbers.Contains(bombNumber))
             {
-                int bombIndex = numbers.IndexOf(bombNumber);
-                int startIndex = bombIndex - bombPower;
-                if (bombIndex == -1)
-                {
-                    break;
-                }
+                
+                int sideBomb = (bombPower*2 + 1) / 2;
+                int startIndex = numbers.IndexOf(bombNumber) - sideBomb;
                 if (startIndex < 0)
                 {
                     startIndex = 0;
                 }
 
-                int count = bombPower * 2 + 1;
-                int lastIndex = numbers.Count - 1;
-                if (bombPower > lastIndex - bombIndex)
+                int endIndex = numbers.IndexOf(bombNumber) + sideBomb;
+                if (endIndex > numbers.Count - 1)
                 {
-                    count = lastIndex - 1;
+                    endIndex = numbers.Count - 1;
                 }
-                numbers.RemoveRange(startIndex, count);
+                
+                for (int i = startIndex; i <= endIndex; i++)
+                {
+                    numbers.RemoveAt(startIndex);
+                }
+                
             }
-
+            
+            int sum = numbers.Sum();
+            Console.WriteLine(sum);
         }
-
     }
 }
