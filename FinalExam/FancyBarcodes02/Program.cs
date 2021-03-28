1aa54a227546071a1ffda0608e6ff75f8dfb1d94
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-03-28 18:48:40 +0300

    up

---
+++ FinalExam/FancyBarcodes02/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace FancyBarcodes02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex barcodeRegex = new Regex(@"^@#+(?<barcode>[A-Z][A-Za-z0-9]{4,}[A-Z])@#+$");
+            Regex digitsRegex = new Regex(@"\d");
+            
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string barcode = Console.ReadLine();
+                
+                Match barcodeMatch = barcodeRegex.Match(barcode);
+                
+                MatchCollection digits = digitsRegex.Matches(barcode);
+
+                if (!barcodeMatch.Success)
+                {
+                    Console.WriteLine($"Invalid barcode");
+                    continue;;
+                }
+                
+                string productGroup = String.Empty;
+
+                if (digits.Count != 0)
+                {
+                    foreach (Match digit in digits)
+                    {
+                        productGroup += digit.Value;
+                    }
+                }
+                else
+                {
+                    productGroup = "00";
+                }
+                
+                Console.WriteLine($"Product group: {productGroup}");
+            }
+
+        }
+    }
+}
