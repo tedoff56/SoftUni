7973d7d7bbb90b1277067447425107518459a5cc
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-03-30 20:59:09 +0300

    exam

---
+++ FinalExam/NeedForSpeedIII 03/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NeedForSpeedIII_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            
+            Dictionary<string, int> mileageByCar = new Dictionary<string, int>();
+            Dictionary<string, int> fuelByCar = new Dictionary<string, int>();
+            
+            for (int i = 0; i < n; i++)
+            {
+                string[] tokens = Console.ReadLine()
+                    .Split('|', StringSplitOptions.RemoveEmptyEntries);
+
+                string car = tokens[0];
+                int mileage = int.Parse(tokens[1]);
+                int fuel = int.Parse(tokens[2]);
+
+                if (mileageByCar.ContainsKey(car))
+                {
+                    continue;
+                }
+                mileageByCar.Add(car, mileage);
+                fuelByCar.Add(car, fuel);
+            }
+
+            while (true)
+            {
+                string line = Console.ReadLine();
+                if (line == "Stop")
+                {
+                    break;
+                }
+
+                string[] tokens = line.Split(" : ", StringSplitOptions.RemoveEmptyEntries);
+
+                string command = tokens[0];
+                string car = tokens[1];
+
+                if (command == "Drive")
+                {
+                    int distance = int.Parse(tokens[2]);
+                    int fuel = int.Parse(tokens[3]);
+
+                    if (!(fuelByCar[car] - fuel > 0))
+                    {
+                        Console.WriteLine("Not enough fuel to make that ride");
+                    }
+                    else
+                    {
+                        mileageByCar[car] += distance;
+                        fuelByCar[car] -= fuel;
+
+                        Console.WriteLine($"{car} driven for {distance} kilometers. {fuel} liters of fuel consumed.");
+                        if (mileageByCar[car] >= 100000)
+                        {
+                            mileageByCar.Remove(car);
+                            fuelByCar.Remove(car);
+
+                            Console.WriteLine($"Time to sell the {car}!");
+                        }
+                    }
+                    
+                }
+                else if (command == "Refuel")
+                {
+                    int fuel = int.Parse(tokens[2]);
+
+                    if (fuelByCar[car] + fuel > 75)
+                    {
+                        fuel = 75 - fuelByCar[car];
+
+                        fuelByCar[car] = 75;
+                    }
+                    else
+                    {
+                        fuelByCar[car] += fuel;
+                    }
+                    
+                    Console.WriteLine($"{car} refueled with {fuel} liters");
+                }
+                else if (command == "Revert")
+                {
+                    int kilometers = int.Parse(tokens[2]);
+
+                    if (mileageByCar[car] - kilometers < 10000)
+                    {
+                        mileageByCar[car] = 10000;
+                    }
+                    else
+                    {
+                        mileageByCar[car] -= kilometers;
+                        Console.WriteLine($"{car} mileage decreased by {kilometers} kilometers");
+                    }
+                }
+                
+            }
+
+            Dictionary<string, int> result = mileageByCar
+                .OrderByDescending(m => m.Value)
+                .ThenBy(n => n.Key)
+                .ToDictionary(k => k.Key, v => v.Value);
+
+            foreach (var kvp in result)
+            {
+                Console.WriteLine($"{kvp.Key} -> Mileage: {kvp.Value} kms, Fuel in the tank: {fuelByCar[kvp.Key]} lt.");
+            }
+        }
+    }
+}
---
+++ FinalExam/SecretChat 01/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace SecretChat_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = Console.ReadLine();
+
+            while (true)
+            {
+                string line = Console.ReadLine();
+                if (line == "Reveal")
+                {
+                    break;
+                }
+
+                string[] tokens = line.Split(":|:");
+
+                string action = tokens[0];
+
+                if (action == "InsertSpace")
+                {
+                    int index = int.Parse(tokens[1]);
+
+                    message = message.Insert(index, " ");
+                    
+                    Console.WriteLine(message);
+                }
+                else if (action == "Reverse")
+                {
+                    string substring = tokens[1];
+
+                    if (message.Contains(substring))
+                    {
+                        int substingIndex = message.IndexOf(substring);
+
+                        message = message.Remove(substingIndex, substring.Length);
+
+                        message = message.Insert(message.Length, Reverse(substring));
+                        
+                        Console.WriteLine(message);
+                    }
+                    else
+                    {
+                        Console.WriteLine("error");
+                    }
+
+                }
+                else if (action == "ChangeAll")
+                {
+                    string substring = tokens[1];
+                    string replacement = tokens[2];
+
+                    while (message.Contains(substring))
+                    {
+                        message = message.Replace(substring, replacement);
+                    }
+                    Console.WriteLine(message);
+                }
+                
+            }
+
+            Console.WriteLine($"You have a new text message: {message}");
+        }
+        private static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
