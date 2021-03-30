8f34ff615c967f7c7cf727ec21628897016503a4
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-03-30 21:02:31 +0300

    Revert "exam"
    This reverts commit 7973d7d7bbb90b1277067447425107518459a5cc.

---
+++ New folder/ReadFromTextFile/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace ReadFromTextFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = System.IO.File
+                .ReadAllText(@"C:\Users\tedof\Documents\GitHub\SoftUni\New folder\ReadFromTextFile\read.txt")
+                .Replace("\n", " ").Replace("\r", " ");
+
+            string[] champions = file.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < champions.Length; i++)
+            {
+                if (Regex.IsMatch(champions[i], "[0-9%]") || Regex.IsMatch(champions[i], "[A-Z][A-Z]"))
+                {
+                    champions[i] = " ";
+                }
+            }
+            
+
+            for (int i = 0; i < champions.Length; i++)
+            {
+                Console.WriteLine($"{champions[i]}");
+            }        
+
+        }
+    }
+}
---
+++ New folder/test/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+        }
+    }
+}
