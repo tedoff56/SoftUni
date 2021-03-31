25ab85fdfa9403ecdc8d884f20da1e4770aa7583
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-03-31 20:42:35 +0300

    up

---
+++ FinalExam/DestinationMapper 02/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace DestinationMapper_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex placesRegex = new Regex(@"(=|\/)(?<place>[A-Z][A-Za-z]{2,})\1");
+
+            string input = Console.ReadLine();
+
+            MatchCollection placesMatches = placesRegex.Matches(input);
+
+            List<string> places = new List<string>();
+            
+            foreach (Match placeMatch in placesMatches)
+            {
+                string place = placeMatch.Groups["place"].Value;
+                places.Add(place);
+            }
+
+            Console.WriteLine($"Destinations: {string.Join(", ", places)}");
+
+            int travelPoints = places.Select(p => p.Length).Sum();
+
+            Console.WriteLine($"Travel Points: {travelPoints}");
+        }
+    }
+}
