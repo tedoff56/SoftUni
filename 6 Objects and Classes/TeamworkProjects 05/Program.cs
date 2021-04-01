4303af6b172f7f52f58a0719cba21a0e2a9485ac
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-04-01 20:10:29 +0300

    up

--- 6 Objects and Classes/TeamworkProjects 05/Program.cs
+++ 6 Objects and Classes/TeamworkProjects 05/Program.cs
@@ -6,48 +6,54 @@ namespace TeamworkProjects_05
 {
     class Team
     {
-        public string Creator { get; set; }
         public string Name { get; set; }
-        public List<string> Users { get; set; }
+
+        public string Creator { get; set; }
+
+        public List<string> Member { get; set; }
         
     }
     class Program
     {
         static void Main(string[] args)
         {
-            int countOfTeams = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
 
-            List<Team> teams = new List<Team>();
+            Team team = new Team();
+
+            Dictionary<string, Team> teamByCreator = new Dictionary<string, Team>();
+            Dictionary<string, Team> teamByName = new Dictionary<string, Team>();
             
-            for (int i = 0; i < countOfTeams; i++)
+            for (int i = 0; i < n; i++)
             {
-                string[] teamInfo = Console.ReadLine()
-                    .Split('-', StringSplitOptions.RemoveEmptyEntries);
+                string[] tokens = Console.ReadLine().Split("-", StringSplitOptions.RemoveEmptyEntries);
 
-                string teamCreator = teamInfo[0];
-                string teamName = teamInfo[1];
-                
-                var teamNameMatch = teams.Where(t => t.Name == teamName);
-                var creatorMatch = teams.Where(t => t.Creator == teamCreator);
-                if (teamNameMatch.Count() != 0)
+                string user = tokens[0];
+                string teamName = tokens[1];
+
+                team = new Team()
+                {
+                    Name = teamName,
+                    Creator = user,
+                    Member = new List<string>()
+                };
+
+                if (teamByName.ContainsKey(teamName))
                 {
                     Console.WriteLine($"Team {teamName} was already created!");
                     continue;
                 }
-                if (creatorMatch.Count() != 0)
+                
+                if (teamByCreator.ContainsKey(user))
                 {
-                    Console.WriteLine($"{teamCreator} cannot create another team!");
+                    Console.WriteLine($"{user} cannot create another team!");
                     continue;
                 }
                 
-                Team team = new Team()
-                {
-                    Creator = teamCreator,
-                    Name = teamName,
-                    Users = new List<string>()
-                };
-                teams.Add(team);
-                Console.WriteLine($"Team {teamName} has been created by {teamCreator}!");
+                teamByCreator.Add(user, team);
+                teamByName.Add(teamName, team);
+                
+                Console.WriteLine($"Team {teamName} has been created by {user}!");
             }
 
             while (true)
@@ -58,65 +64,68 @@ namespace TeamworkProjects_05
                     break;
                 }
 
-                string[] joinData = line.Split("->", StringSplitOptions.RemoveEmptyEntries);
-                string user = joinData[0];
-                string teamName = joinData[1];
-                var teamNameMatch = teams.Where(t => t.Name == teamName);
-                var teamUsersMatch = teams.Where(t => t.Users.Contains(user));
-                var teamCreatorMatch = teams.Where(t => t.Creator == user);
+                string[] tokens = line.Split("->", StringSplitOptions.RemoveEmptyEntries);
 
-                if (teamNameMatch.Count() == 0)
+                string user = tokens[0];
+                string teamName = tokens[1];
+
+                if (!teamByName.ContainsKey(teamName))
                 {
                     Console.WriteLine($"Team {teamName} does not exist!");
                     continue;
                 }
 
-                if (teamUsersMatch.Count() > 0)
-                {
-                    Console.WriteLine($"Member {user} cannot join team {teamName}!");
-                    continue;
-                }
-                else if (teamCreatorMatch.Count() > 0)
+                if (MemberAlreadyInTeam(teamByName, user))
                 {
                     Console.WriteLine($"Member {user} cannot join team {teamName}!");
                     continue;
                 }
 
-                foreach (var team in teams)
-                {
-                    if (team.Name == teamName)
-                    {
-                        team.Users.Add(user);
-                    }
-                }
-                
+                teamByName[teamName].Member.Add(user);
+
             }
 
-            var validTeams = teams
-                .Where(t => t.Users.Count() != 0)
-                .OrderByDescending(t=>t.Users.Count())
-                .ThenBy(t=>t.Name);
-            var invalidTeams = teams.Where(t => t.Users.Count() == 0);
+            Dictionary<string, Team> validTeams = teamByName
+                .Where(t => t.Value.Member.Count() != 0)
+                .OrderByDescending(t=>t.Value.Member.Count())
+                .ThenBy(t=>t.Value.Name)
+                .ToDictionary(k => k.Key, v => v.Value);
 
-            foreach (var team in validTeams)
+            Dictionary<string, Team> invalidTeams = teamByName
+                .Where(t => t.Value.Member.Count() == 0)
+                .OrderBy(t=>t.Value.Name)
+                .ToDictionary(k => k.Key, v => v.Value);
+            
+            foreach (var kvp in validTeams)
             {
-                string teamName = team.Name;
-                string creator = team.Creator;
-                
-                List<string> users = team.Users.OrderBy(t=> t).ToList();
-                Console.WriteLine($"{teamName}");
-                Console.WriteLine($"- {creator}");
-                foreach (var user in users)
+                Console.WriteLine(kvp.Key);
+                Console.WriteLine($"- {kvp.Value.Creator}");
+                foreach (var member in kvp.Value.Member.OrderBy(m=>m))
                 {
-                    Console.WriteLine($"-- {user}");
+                    Console.WriteLine($"-- {member}");
                 }
             }
 
             Console.WriteLine("Teams to disband:");
-            foreach (var team in invalidTeams)
+            
+            foreach (var kvp in invalidTeams)
             {
-                Console.WriteLine($"{team.Name}");
+                Console.WriteLine(kvp.Key);
             }
+        }
+
+        private static bool MemberAlreadyInTeam(Dictionary<string, Team> team, string user)
+        {
+            foreach (var kvp in team)
+            {
+                List<string> members = kvp.Value.Member;
+                if (members.Contains(user) || kvp.Value.Creator == user)
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
