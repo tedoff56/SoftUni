8329a75f90c15cca47bfea40aa4b8ca043a61947
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-04-17 18:57:51 +0300

    up

---
+++ More Exercises/MultiplicationSign 05/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace MultiplicationSign_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int negativeCnt = 0;
+            
+            for (int i = 0; i < 3; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (number == 0)
+                {
+                    negativeCnt = -1;
+                    break;
+                }
+                
+                if(number < 0)
+                {
+                    negativeCnt++;
+                }
+            }
+
+            if (negativeCnt != -1)
+            {
+                if (negativeCnt % 2 == 0)
+                {
+                    Console.WriteLine("positive");
+                }
+                else
+                {
+                    Console.WriteLine("negative");
+                }
+            }
+            else
+            {
+                Console.WriteLine("zero");
+            }
+
+        }
+        
+    }
+}
