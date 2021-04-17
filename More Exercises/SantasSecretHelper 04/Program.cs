864d63e68cf3ddca507022feed68ef32b62b53c2
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-04-17 18:56:38 +0300

    up

---
+++ More Exercises/SantasSecretHelper 04/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace SantasSecretHelper_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int key = int.Parse(Console.ReadLine());
+            
+            Regex regex = new Regex(@"@(?<name>[A-Za-z]+)[^@\-!:>]*!(?<behviour>G|N)!");
+            
+            while (true)
+            {
+                StringBuilder line = new StringBuilder(Console.ReadLine());
+
+                if (line.Equals("end"))
+                {
+                    break;
+                }
+                
+                for (int i = 0; i < line.Length; i++)
+                {
+                    line[i] = (char)(line[i] - key) ;
+                }
+                
+                Match match = regex.Match(line.ToString());
+
+                if(!match.Success)
+                {
+                    continue;
+                }
+
+                if (match.Groups["behviour"].Value == "N")
+                {
+                    continue;
+                }
+
+                Console.WriteLine(match.Groups["name"]);
+                
+            }
+        }
+    }
+}
