29863e05060e4cc6944b77abf173acab16dc0ed5
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-05-17 20:15:52 +0300

    up

---
+++ C# Advanced/Old Courses/_01 StacksAndQueues/_07TruckTour/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07TruckTour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Queue<int[]> pumps = new Queue<int[]>();
+            
+            for (int i = 0; i < n; i++)
+            {
+                int[] data = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                
+                pumps.Enqueue(data);
+            }
+
+            bool hasPetrol = true;
+            
+            for (int i = 0; i < n; i++)
+            {
+                int currentPetrol = 0;
+
+                for (int j = 0; j < n; j++)
+                {
+                    int[] pump = pumps.Dequeue();
+                    pumps.Enqueue(pump);
+                    
+                    currentPetrol += pump[0];
+                    currentPetrol -= pump[1];
+
+                    if (currentPetrol < 0)
+                    {
+                        hasPetrol = false;
+                    }
+                }
+
+                pumps.Enqueue(pumps.Peek());
+                pumps.Dequeue();
+
+                if (hasPetrol == true)
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+
+                hasPetrol = true;
+            }
+        }
+    }
+}
---
+++ C# Advanced/Old Courses/_01 StacksAndQueues/_08BalancedParentheses/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08BalancedParentheses
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var line = Console.ReadLine();
+
+            var openBrackets = new Stack<char>();
+
+            bool isValid = true;
+            foreach (var bracket in line)
+                if (bracket == '{' || bracket == '[' || bracket == '(')
+                {
+                    openBrackets.Push(bracket);
+                }
+                else if (!openBrackets.Any())
+                {
+                    isValid = false;
+                    break;
+                }
+                else
+                {
+                    bool firstIsValid = bracket == '}' && openBrackets.Pop() == '{';
+                    bool secondIsValid = bracket == ']' && openBrackets.Pop() == '[';
+                    bool thirdIsValid = bracket == ')' && openBrackets.Pop() == '(';
+
+                    if (!firstIsValid && !secondIsValid && !thirdIsValid)
+                    {
+                        isValid = false;
+                        break;
+                    }
+                }
+
+            if (isValid)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
