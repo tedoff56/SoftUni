30204f3fa0b3b0d43fc72440b398b438a54928ba
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-05-21 22:27:00 +0300

    up

---
+++ C# Advanced/_01 StacksAndQueues/_10Crossroads/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _10Crossroads
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var greenLightDuration = int.Parse(Console.ReadLine());
+            var freeWindowDuration = int.Parse(Console.ReadLine());
+
+            var carsQueue = new Queue<string>();
+
+            var totalCarsPassed = 0;
+
+            while (true)
+            {
+                var greenLight = greenLightDuration;
+                var freeWindow = freeWindowDuration;
+
+                var command = Console.ReadLine();
+
+                if (command == "END")
+                {
+                    Console.WriteLine("Everyone is safe.");
+                    Console.WriteLine($"{totalCarsPassed} total cars passed the crossroads.");
+
+                    return;
+                }
+
+                if (command == "green")
+                {
+                    while (greenLight > 0 && carsQueue.Count > 0)
+                    {
+                        var firstCar = carsQueue.Dequeue();
+
+                        greenLight -= firstCar.Length;
+
+                        if (greenLight > 0)
+                        {
+                            totalCarsPassed++;
+                        }
+                        else
+                        {
+                            freeWindow += greenLight;
+
+                            if (freeWindow < 0)
+                            {
+                                var characterHit = firstCar[freeWindow + firstCar.Length];
+
+                                Console.WriteLine("A crash happened!");
+                                Console.WriteLine($"{firstCar} was hit at {characterHit}.");
+                                return;
+                            }
+
+                            totalCarsPassed++;
+                        }
+                    }
+                }
+                else
+                {
+                    carsQueue.Enqueue(command);
+                }
+            }
+        }
+    }
+}
---
+++ C# Advanced/_01 StacksAndQueues/_11KeyRevolver/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _11KeyRevolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bulletPrice = int.Parse(Console.ReadLine());
+            int gunBarrelSize = int.Parse(Console.ReadLine());
+
+            Stack<int> bullets = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+            Queue<int> locks = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+
+            int intelligence = int.Parse(Console.ReadLine());
+
+            int bulletsCount = 0;
+            int totalBulletsShot = 0;
+            
+            while (locks.Count > 0)
+            {
+                if (bullets.Count == 0)
+                {
+                    Console.WriteLine($"Couldn't get through. Locks left: {locks.Count}");
+                    return;
+                }
+                
+                int currentBullet = bullets.Pop();
+                int currentLock = locks.Peek();
+
+                if (currentBullet <= currentLock)
+                {
+                    Console.WriteLine("Bang!");
+                    locks.Dequeue();
+                }
+                else
+                {
+                    Console.WriteLine("Ping!");
+                }
+                
+                bulletsCount++;
+                totalBulletsShot++;
+                
+                if (bulletsCount == gunBarrelSize && bullets.Count > 0) 
+                {
+                    bulletsCount = 0;
+                    Console.WriteLine("Reloading!");
+                }
+            }
+
+            Console.WriteLine($"{bullets.Count} bullets left. Earned ${intelligence - (totalBulletsShot * bulletPrice)}");
+
+        }
+    }
+}
