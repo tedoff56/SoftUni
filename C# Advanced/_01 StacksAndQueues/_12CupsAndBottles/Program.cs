b614e26f0ec56bc571eb766349e78a5228b38fe3
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-05-22 14:17:37 +0300

    multidimensional arrays

---
+++ C# Advanced/_01 StacksAndQueues/_12CupsAndBottles/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _12CupsAndBottles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] cupsCapacity = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] filledBottles = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> cupsCapacityQueue = new Queue<int>(cupsCapacity);
+            Stack<int> filledBottlesStack = new Stack<int>(filledBottles);
+
+            while (true)
+            {
+                int filledBottleCapacity = filledBottlesStack.Peek();
+                int cupCapacity = cupsCapacityQueue.Peek();
+
+                cupCapacity -= filledBottleCapacity;
+                
+                if (filledBottleCapacity > cupCapacity)
+                {
+                    if (cupCapacity <= 0)
+                    {
+                        cupsCapacityQueue.Dequeue();
+                    }
+                }
+                else
+                {
+                    filledBottlesStack.Pop();
+                }
+                
+
+            }
+        }
+    }
+}
---
+++ C# Advanced/_02 MultidimensionalArrays/_01DiagonalDifference/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+
+namespace _01DiagonalDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+
+            FillMatrix(n, matrix);
+            
+            int firstDiagonalSum = GetFirstDiagonal(n, matrix);
+            int secondDiagonalSum = GetSecondDiagonal(n, matrix);
+            
+            Console.WriteLine(Math.Abs(firstDiagonalSum - secondDiagonalSum));
+        }
+
+        static void FillMatrix(int n, int[,] matrix)
+        {
+            for (int row = 0; row < n; row++)
+            {
+                int[] rowNumbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = rowNumbers[col];
+                }
+            }
+        }
+        static int GetFirstDiagonal(int n, int[,] matrix)
+        {
+            int sum = 0;
+            for (int row = 0; row < n; row++)
+            {
+                sum += matrix[row, row];
+            }
+
+            return sum;
+        }
+        
+        static int GetSecondDiagonal(int n, int[,] matrix)
+        {
+            int sum = 0;
+            for (int i = n - 1; i >= 0; i--)
+            {
+                int j = Math.Abs(i - n + 1);
+                sum += matrix[i, j];
+            }
+            
+            return sum;
+        }
+    }
+}
