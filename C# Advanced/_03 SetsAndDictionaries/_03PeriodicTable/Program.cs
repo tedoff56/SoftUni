f5695e3c7cce6dde86c73d6e5fd4051e7ac8ab92
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-05-28 14:59:14 +0300

    up

---
+++ C# Advanced/_03 SetsAndDictionaries/_03PeriodicTable/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03PeriodicTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            SortedSet<string> uniqueElements = new SortedSet<string>();
+            
+            for (int i = 0; i < n; i++)
+            {
+                string[] elements = Console.ReadLine().Split();
+
+                foreach (var element in elements)
+                {
+                    uniqueElements.Add(element);
+                }
+            }
+
+            Console.WriteLine(string.Join(' ', uniqueElements));
+        }
+    }
+}
