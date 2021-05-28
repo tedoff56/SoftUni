4e439f9bb9823f685f747e587f62d5baf3605513
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-05-28 14:52:03 +0300

    up

--- C# Advanced/_02 MultidimensionalArrays/_08Bombs/Program.cs
+++ C# Advanced/_02 MultidimensionalArrays/_08Bombs/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Linq;
 
-
 namespace _08Bombs
 {
     class Program
---
+++ C# Advanced/_03 SetsAndDictionaries/_02SetsOfElements/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02SetsOfElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] lengths = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            int n = lengths[0];
+            int m = lengths[1];
+            
+            HashSet<int> firstSet = new HashSet<int>();
+            HashSet<int> secondSet = new HashSet<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                firstSet.Add(num);
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                secondSet.Add(num);
+            }
+
+            foreach (var number in firstSet)
+            {
+                if (secondSet.Contains(number))
+                {
+                    Console.Write(number + " ");
+                }
+            }
+        }
+    }
+}
