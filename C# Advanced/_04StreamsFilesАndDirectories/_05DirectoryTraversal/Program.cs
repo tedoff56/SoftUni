3d97a20d7388cf6b4bea9c5b56e116ff72759b0a
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-06-04 19:10:52 +0300

    up

--- C# Advanced/_04StreamsFilesАndDirectories/_05DirectoryTraversal/Program.cs
+++ C# Advanced/_04StreamsFilesАndDirectories/_05DirectoryTraversal/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace _05DirectoryTraversal
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            string path = Console.ReadLine();
+
+            string[] files = Directory.GetFiles(path);
+
+            Dictionary<string, List<FileInfo>> filesByExtension = new Dictionary<string, List<FileInfo>>();
             
+            foreach (var file in files)
+            {
+                FileInfo fileInfo = new FileInfo(file);
+                string fileExtension = fileInfo.Extension;
+
+                if (!filesByExtension.ContainsKey(fileExtension))
+                {
+                    filesByExtension.Add(fileExtension, new List<FileInfo>());
+                }
+
+                filesByExtension[fileExtension].Add(fileInfo);
+            }
+
+            string outPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "/report.txt";
+            foreach (var kvp in filesByExtension
+                .OrderByDescending(f=>f.Value.Count)
+                .ThenBy(f=>f.Key))
+            {
+                await File.AppendAllTextAsync(outPath, kvp.Key + Environment.NewLine);
+                
+                foreach (var file in kvp.Value.OrderBy(f=>Math.Ceiling((double)f.Length / 1024)))
+                {
+                    await File.AppendAllTextAsync(outPath, $"--{file.Name} - {Math.Ceiling((double)file.Length / 1024)}kb" + Environment.NewLine);
+                }
+            }
         }
     }
 }
---
+++ C# Advanced/_04StreamsFilesАndDirectories/_06ZipAndExtract/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO.Compression;
+
+namespace _06ZipAndExtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string startPath = @"../../../zipFolder";
+            string zipPath = @"../../../zipFile.zip";
+            string extractPath = @"../../../extract";
+
+            ZipFile.CreateFromDirectory(startPath, zipPath);
+            
+            ZipFile.ExtractToDirectory(zipPath, extractPath);
+        }
+    }
+}
