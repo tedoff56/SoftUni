a86e0ad7e5f0f51196192521c04b9ce734c9529b
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-06-06 15:23:08 +0300

    up

---
+++ C# Advanced/_05 FunctionalProgramming/_12TriFunction/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace _12TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string[] names = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Func<string, int, bool> charSum = (name, number) => name.ToCharArray().Sum(c => c) >= number;
+
+            if(names.Length > 0)
+            {
+                Console.WriteLine(Action(names, charSum, n)[0]);
+            }
+
+        }
+        private static string[] Action(string[] names, Func<string, int, bool> func, int num)
+        {
+            return names.Where(name => func(name, num)).ToArray();
+        }
+    }
+}
