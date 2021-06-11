1d526b9971730ea66142538b2f04b8b68abe2e73
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-06-11 18:59:11 +0300

    up

---
+++ C# Advanced/_06 DefiningClasses/_09PokemonTrainer/StartUp.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09PokemonTrainer
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var trainers = GetTrainers();
+
+            string element = Console.ReadLine();
+            while (element?.ToLower() != "end")
+            {
+                foreach (var trainer in trainers)
+                {
+                    if (trainer.CheckPokemonsElements(element))
+                    {
+                        trainer.NumberOfBadges++;
+                    }
+                    else
+                    {
+                        trainer.RemoveHealth();
+                    }
+                }
+            
+                element = Console.ReadLine();
+            }
+            
+            foreach (var trainer in trainers.OrderByDescending(t => t.NumberOfBadges))
+            {
+                Console.WriteLine(trainer);
+            }
+        }
+
+        private static List<Trainer> GetTrainers()
+        {
+            List<Trainer> trainers = new List<Trainer>();
+
+            string line = Console.ReadLine();
+            while (line?.ToLower() != "tournament")
+            {
+                string[] data = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string trainerName = data[0];
+                
+                if (!trainers.Any(t => t.Name == trainerName))
+                {
+                    trainers.Add(new Trainer(trainerName));
+                }
+
+                Trainer findTrainer = trainers.Find(t => t.Name == trainerName);
+
+                findTrainer.CatchPokemon(new Pokemon(data[1], data[2], int.Parse(data[3])));
+
+                line = Console.ReadLine();
+            }
+
+            return trainers;
+        }
+    }
+}
---
+++ C# Advanced/_06 DefiningClasses/_09PokemonTrainer/Trainer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09PokemonTrainer
+{
+    public class Trainer
+    {
+        
+        public Trainer(string name)
+        {
+            this.Name = name;
+        }
+        public string Name { get; set; }
+
+        public int NumberOfBadges { get; set; }
+
+        public List<Pokemon> Pokemons { get; private set; } = new List<Pokemon>();
+
+        public void CatchPokemon(Pokemon pokemon) => this.Pokemons.Add(pokemon);
+        
+        public bool CheckPokemonsElements(string element) => this.Pokemons.Exists(p => p.Element == element);
+
+        public void RemoveHealth()
+        {
+            for (int i = 0; i < this.Pokemons.Count; i++)
+            {
+                this.Pokemons[i].Health -= 10;
+                if (this.Pokemons[i].Health <= 0)
+                {
+                    this.Pokemons.Remove(this.Pokemons[i]);
+                }
+            }
+        }
+
+        public override string ToString() => $"{this.Name} {this.NumberOfBadges} {this.Pokemons.Count}";
+    }
+}
