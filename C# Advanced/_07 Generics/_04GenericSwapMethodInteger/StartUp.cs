a80efff94ef4707cf37f069654dbb226e377a395
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-06-12 20:08:00 +0300

    up

---
+++ C# Advanced/_07 Generics/_04GenericSwapMethodInteger/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace _04GenericSwapMethodInteger
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Swaper<int> swaper = new Swaper<int>();
+            
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                swaper.Add(number);
+            }
+
+            int[] indexes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            swaper.Swap(indexes[0], indexes[1]);
+
+            Console.WriteLine(swaper);
+        }
+    }
+}
