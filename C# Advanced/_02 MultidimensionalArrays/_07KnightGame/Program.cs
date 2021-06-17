f3a8e6957245bd631d5c60f6eb34c4b2dceece1a
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-06-17 21:42:17 +0300

    up

---
+++ C# Advanced/_02 MultidimensionalArrays/_07KnightGame/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Linq;
+
+namespace _07KnightGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine() ?? string.Empty);
+
+            char[][] board = new char[size][];
+
+            for (int i = 0; i < size; i++)
+            {
+                string row = Console.ReadLine();
+
+                board[i] = row.ToArray();
+            }
+
+            int minimumKnights = int.MaxValue;
+            
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    int toBeRemoved = 0;
+                    
+                    if (board[i][j] == '0')
+                    {
+                        continue;
+                    }
+
+                    if (isKnight(i - 1, j + 2, board))
+                    {
+                        toBeRemoved++;
+                    }
+                    else if (isKnight(i + 1, j + 2, board))
+                    {
+                        toBeRemoved++;
+                    }
+                    else if (isKnight(i - 1, j - 2, board))
+                    {
+                        toBeRemoved++;
+                    }
+                    else if (isKnight(i + 1, j - 2, board))
+                    {
+                        toBeRemoved++;
+                    }
+                    else if (isKnight(i - 2, j + 1, board))
+                    {
+                        toBeRemoved++;
+                    }
+                    else if (isKnight(i + 2, j + 1, board))
+                    {
+                        toBeRemoved++;
+                    }
+                    else if (isKnight(i - 2, j - 1, board))
+                    {
+                        toBeRemoved++;
+                    }
+                    else if (isKnight(i + 2, j - 1, board))
+                    {
+                        toBeRemoved++;
+                    }
+
+                    if (toBeRemoved < minimumKnights)
+                    {
+                        minimumKnights = toBeRemoved;
+                    }
+                }
+            }
+
+            Console.WriteLine(minimumKnights);
+            
+        }
+        
+        private static bool isKnight(int row, int col, char[][] board)
+        {
+            bool isIndexValid = row >= 0 && row < board.GetLength(0) && 
+                                col >= 0 && col < board.GetLength(1);
+            
+            return isIndexValid && board[row][col] == 'K';
+        }
+
+
+    }
+}
---
+++ C# Advanced/_08IteratorsAndComparators/_01ListyIterator/ListyIterator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _01ListyIterator
+{
+    public class ListyIterator<T> : IEnumerable<T>
+    {
+        private List<T> _list;
+        private int _index;
+
+        public ListyIterator(params T[] elements)
+        {
+            _list = new List<T>(elements);
+            _index = 0;
+        }
+        public List<T> List1 => _list;
+        
+        public bool HasNext()
+        {
+            if (_index + 1 < _list.Count)
+            {
+                return true;
+            }
+
+            return false;
+        }
+        
+        public bool Move()
+        {
+            if (HasNext())
+            {
+                _index++;
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Print()
+        {
+            if (_list.Count == 0)
+            {
+                throw new ArgumentException("Invalid Operation!");
+            }
+
+            Console.WriteLine(_list[_index]);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var element in _list)
+            {
+                yield return element;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
