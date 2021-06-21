bddc74e191c01a3a4497d43434bcdea986cc5c55
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-06-21 22:13:38 +0300

    up

---
+++ C# Advanced/ExamPrep/Scheduling/StartUp.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Scheduling
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            int[] tasks = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            int[] threads = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            int taskToKill = int.Parse(Console.ReadLine());
+
+            var tasksStack = new Stack<int>(tasks);
+            var threadsQueue = new Queue<int>(threads);
+
+            int value = 0;
+            while (true)
+            {
+                int currentTask = tasksStack.Peek();
+                int currentThread = threadsQueue.Peek();
+                
+                if (currentTask == taskToKill)
+                {
+                    value = currentThread;
+                    break;
+                }
+                
+                if (currentThread >= currentTask)
+                {
+                    tasksStack.Pop();
+                    threadsQueue.Dequeue();
+                }
+                else
+                {
+                    threadsQueue.Dequeue();
+                }
+                
+            }
+
+            Console.WriteLine($"Thread with value {value} killed task {taskToKill}");
+            Console.WriteLine(string.Join(' ', threadsQueue));
+        }
+    }
+}
