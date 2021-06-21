72dfc18cbd763d82c4464b06c5bfde03f0eee227
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-06-21 19:51:17 +0300

    up

---
+++ C# Advanced/_08 IteratorsAndComparators/_03Stack/MyStack.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _03Stack
+{
+    public class MyStack<T> : IEnumerable<T>
+    {
+        private List<T> _elements;
+
+        public MyStack()
+        {
+            _elements = new List<T>();
+        }
+        
+        public void Push(params T[] elements)
+        {
+            for (int i = 0; i < elements.Length; i++)
+            {
+                _elements.Add(elements[i]);
+            }
+        }
+
+        public T Pop()
+        {
+            if (_elements.Count == 0)
+            {
+                Console.WriteLine("No elements");
+                return default;
+            }
+            
+            T element = _elements[_elements.Count - 1];
+            
+            _elements.RemoveAt(_elements.Count - 1);
+
+            return element;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = _elements.Count - 1; i >= 0 ; i--)
+            {
+                yield return _elements[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ C# Advanced/_08 IteratorsAndComparators/_03Stack/StartUp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace _03Stack
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            MyStack<int> myStack = new MyStack<int>();
+            
+            string command = Console.ReadLine();
+            while (command != "END")
+            {
+                if (command.Contains("Push"))
+                {
+                    int[] tokens = command
+                        .Split(new[] {' ', ','}, StringSplitOptions.RemoveEmptyEntries)
+                        .Skip(1).Select(int.Parse).ToArray();
+                    
+                    myStack.Push(tokens);
+                }
+                else if (command == "Pop")
+                {
+                    myStack.Pop();
+                }
+                
+                command = Console.ReadLine();
+            }
+
+            foreach (int element in myStack)
+            {
+                Console.WriteLine(element);
+            }
+            
+            foreach (int element in myStack)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
