08c1f5cb0b5062f41ffda0c678bea7805bba02a9
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-06-29 22:46:05 +0300

    animals

---
+++ C# OOP/_01 Inheritance/Animals/Animal.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Animals
+{
+    public abstract class Animal
+    {
+        private int _age;
+        private string _gender;
+        
+        public Animal(string name, int age, string gender)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+        }
+
+        public string Name { get; set; }
+
+        public int Age 
+        {
+            get => _age;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+
+                _age = value;
+            }
+        }
+
+        public string Gender
+        {
+            get => _gender;
+            set
+            {
+                bool isValid = value == "Male" || value == "Female";
+                if(!isValid)
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+
+                _gender = value;
+            }
+        }
+
+        public abstract string ProduceSound();
+    }
+}
--- C# OOP/_01 Inheritance/Animals/StartUp.cs
+++ C# OOP/_01 Inheritance/Animals/StartUp.cs
@@ -1,10 +1,58 @@
-﻿namespace Animals
+﻿using System;
+using System.Text;
+
+namespace Animals
 {
     public class StartUp
     {
         public static void Main(string[] args)
         {
             
+            StringBuilder sb = new StringBuilder();
+            
+            while (true)
+            {
+                string animalType = Console.ReadLine();
+                string[] animalData = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                if (animalType == "Beast!")
+                {
+                    Console.WriteLine(sb.ToString().Trim());
+                    break;
+                }
+                
+                Animal animal;
+                
+                string name = animalData[0];
+                int age = int.Parse(animalData[1]);
+                string gender = animalData[2];
+
+ 
+                switch (animalType)
+                {
+                    case "Cat":
+                        animal = new Cat(name, age, gender);
+                        break;
+                    case "Tomcat":
+                        animal = new Tomcat(name, age);
+                        break;
+                    case "Kitten":
+                        animal = new Kitten(name, age);
+                        break;
+                    case "Dog":
+                        animal = new Dog(name, age, gender);
+                        break;
+                    case "Frog":
+                        animal = new Frog(name, age, gender);
+                        break;
+                    default:
+                        throw new ArgumentException("Invalid input!");
+                }
+
+                sb.AppendLine($"{animalType}")
+                    .AppendLine($"{animal.Name} {animal.Age} {animal.Gender}")
+                    .AppendLine(animal.ProduceSound());
+            }
         }
     }
 }
