8f97630ab1f306e9cb368342c38526de0b0b38e1
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-06-29 21:13:50 +0300

    need for speed

--- C# OOP/_01 Inheritance/NeedForSpeed/StartUp.cs
+++ C# OOP/_01 Inheritance/NeedForSpeed/StartUp.cs
@@ -1,10 +1,17 @@
-﻿namespace NeedForSpeed
+﻿using System;
+
+namespace NeedForSpeed
 {
     public class StartUp
     {
         public static void Main(string[] args)
         {
+            SportCar car = new SportCar(100, 50);
             
+            car.Drive(10);
+            Console.WriteLine(car.Fuel);
+            car.Drive(10);
+            Console.WriteLine(car.Fuel);
         }
     }
 }
