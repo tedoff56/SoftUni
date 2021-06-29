bafbb8777b4df7bb5a868718aaddb01a6025f000
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-06-29 21:38:19 +0300

    restaurant

---
+++ C# OOP/_01 Inheritance/Restaurant/Coffee.cs
@@ -0,0 +1,16 @@
+﻿namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        private const double DefaultMilliliters = 50;
+        private const decimal DefaultPrice = 3.5M;
+        
+        public Coffee(string name, double caffeine) 
+            : base(name, DefaultPrice, DefaultMilliliters)
+        {
+            Caffeine = caffeine;
+        }
+        
+        public double Caffeine { get; set; }
+    }
+}
