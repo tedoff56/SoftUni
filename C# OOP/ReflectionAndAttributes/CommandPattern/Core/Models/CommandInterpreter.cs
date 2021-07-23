99b9068e5f91fa2c002acc572475ad48b265555e
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-07-23 19:09:02 +0300

    reflection up

---
+++ C# OOP/ReflectionAndAttributes/CommandPattern/Core/Models/CommandInterpreter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using CommandPattern.Core.Contracts;
+
+namespace CommandPattern.Core.Models
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        private const string CommandSuffix = "Command";
+            
+        public string Read(string args)
+        {
+            string[] tokens = args.Split();
+
+            string commandName = tokens[0];
+            string[] commandArgs = tokens[1..];
+
+            Type commandType = Assembly
+                .GetCallingAssembly()
+                .GetTypes()
+                .FirstOrDefault(t => t.Name == $"{commandName}{CommandSuffix}");
+            
+            ICommand command = Activator.CreateInstance(commandType) as ICommand;
+            
+            
+            return command?.Execute(commandArgs);
+        }
+    }
+}
---
+++ C# OOP/ReflectionAndAttributes/CommandPattern/Core/Models/Engine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using CommandPattern.Core.Contracts;
+
+namespace CommandPattern.Core.Models
+{
+    public class Engine : IEngine
+    {
+        private ICommandInterpreter _commandInterpreter;
+        public Engine(ICommandInterpreter command)
+        {
+            this._commandInterpreter = command;
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                string result = _commandInterpreter.Read(command);
+
+                if (result == null)
+                {
+                    break;
+                }
+                
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
