d656281457e9bdf2f4302683119d8e0061e2cb71
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-07-23 19:57:38 +0300

    up

---
+++ C# OOP/ReflectionAndAttributes/ValidationAttributes/MyRangeAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ValidationAttributes
+{
+    public class MyRangeAttribute : MyValidationAttribute
+    {
+        private int _minValue;
+        private int _maxValue;
+        
+        public MyRangeAttribute(int minValue, int maxValue)
+        {
+            this._minValue = minValue;
+            this._maxValue = maxValue;
+        }
+        
+        public override bool IsValid(object obj)
+        {
+            bool isInt = obj is int;
+            if (!isInt)
+            {
+                throw new ArgumentException();
+            }
+
+            int value = (int) obj;
+
+            bool isInRange = value >= _minValue && value <= _maxValue;
+            if (!isInRange)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ C# OOP/ReflectionAndAttributes/ValidationAttributes/Validator.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+
+namespace ValidationAttributes
+{
+    public static class Validator   
+    {
+        public static bool IsValid(object obj)
+        {
+            PropertyInfo[] props = obj.GetType().GetProperties();
+
+            foreach (PropertyInfo property in props)
+            {
+                var attribute = property.GetCustomAttribute(typeof(MyValidationAttribute), false) as MyValidationAttribute;
+
+                bool isValid = attribute.IsValid(property.GetValue(obj));
+                if (!isValid)
+                {
+                    return false;
+                }
+                
+            }
+
+            return true;
+        }
+    }
+}
