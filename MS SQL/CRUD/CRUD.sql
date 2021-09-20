84d5c84a9ff972ba75f329e87e97b15f39ad6c7b
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-09-20 22:31:15 +0300

    up

--- MS SQL/CRUD/CRUD.sql
+++ MS SQL/CRUD/CRUD.sql
@@ -29,3 +29,37 @@ SELECT CONCAT([FirstName], ' ', [MiddleName], ' ', [LastName]) AS [Full Name]
   FROM [Employees]
  WHERE [Salary] IN (25000, 14000, 12500, 23600)
 
+ SELECT [FirstName], [LastName] 
+   FROM [Employees]
+  WHERE [ManagerID] IS NULL
+
+  SELECT [FirstName], [LastName], [Salary] 
+    FROM [Employees]
+   WHERE [Salary] > 50000
+ORDER BY [Salary] DESC
+
+   SELECT TOP (5) [FirstName], [LastName] 
+    FROM  [Employees]
+ ORDER BY [Salary] DESC
+
+ SELECT [FirstName], [LastName] 
+   FROM [Employees]
+  WHERE [DepartmentID] <> 4
+
+  SELECT * 
+    FROM [Employees]
+ORDER BY [Salary] DESC, [FirstName] ASC, [LastName] DESC, [MiddleName] ASC 
+
+
+CREATE VIEW [V_EmployeesSalariesy] AS (
+     SELECT [FirstName], [LastName], [Salary] 
+       FROM [Employees])
+
+SELECT * FROM [V_FirstLastNameSalary]
+
+CREATE VIEW [V_EmployeeNameJobTitle] AS(
+SELECT CONCAT([FirstName], ' ', ISNULL([MiddleName], ''), ' ', [LastName]) AS [Full Name], 
+       [JobTitle] AS [Job Title]
+  FROM [Employees])
+
+SELECT * FROM [V_EmployeeNameJobTitle]
