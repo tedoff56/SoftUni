8947d9c7e4da6cc685668ad612a5e733325f66ab
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-09-23 19:53:14 +0300

    up

--- MS SQL/Table Relations/FirstProblems.sql
+++ MS SQL/Table Relations/FirstProblems.sql
@@ -25,3 +25,29 @@ INSERT INTO [Persons]([FirstName], [Salary], [PassportID])
 		('Roberto', 43300.00, 102),
 		('Tom', 56100.00, 103),
 		('Yana', 60200.00, 101)
+
+
+CREATE TABLE [Manufacturers](
+	[ManufacturerID] INT IDENTITY PRIMARY KEY,
+	[Name] VARCHAR(50) UNIQUE NOT NULL,
+	[EstablishedOn] DATE NOT NULL
+)
+
+CREATE TABLE [Models](
+	[ModelID] INT IDENTITY(101, 1) PRIMARY KEY,
+	[Name] VARCHAR(50) NOT NULL,
+	[ManufacturerID] INT FOREIGN KEY REFERENCES [Manufacturers]([ManufacturerID]) NOT NULL
+)
+
+INSERT INTO [Manufacturers]([Name], [EstablishedOn]) VALUES
+	('BMW', '07/03/1916'),
+	('Tesla', '01/01/2003'),
+	('Lada', '01/05/1966')
+
+INSERT INTO [Models]([Name], [ManufacturerID]) VALUES
+	('X1', 1),
+	('i6', 1),
+	('Model S', 2),
+	('Model X', 2),
+	('Model 3', 2),
+	('Nova', 3)
