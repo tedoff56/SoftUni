b2c4701e854fb24db5a3cfb666098a934d3afe43
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-09-23 20:24:19 +0300

    up

--- MS SQL/Table Relations/FirstProblems.sql
+++ MS SQL/Table Relations/FirstProblems.sql
@@ -89,3 +89,19 @@ INSERT INTO [StudentsExams]([StudentID], [ExamID]) VALUES
 	(3, 103),
 	(2, 102),
 	(2, 103)
+
+--4
+
+CREATE TABLE [Teachers](
+	[TeacherID] INT IDENTITY(101, 1) PRIMARY KEY,
+	[Name] NVARCHAR(80) NOT NULL,
+	[ManagerID] INT FOREIGN KEY REFERENCES [Teachers]([TeacherID])
+)
+
+INSERT INTO [Teachers]([Name], [ManagerID]) VALUES
+	('John', NULL),
+	('Maya', 106),
+	('Silvia', 106),
+	('Ted', 105),
+	('Mark', 101),
+	('Greta', 101)
---
+++ MS SQL/Table Relations/OnlineStoreDatabase.sql
@@ -0,0 +1,49 @@
+--5
+CREATE DATABASE [OnlineShop]
+
+USE [OnlineShop]
+
+CREATE TABLE [ItemsTypes](
+	[ItemTypeID] INT IDENTITY PRIMARY KEY,
+	[Name] VARCHAR(50) UNIQUE NOT NULL 
+)
+
+CREATE TABLE [Items](
+	[ItemID] INT IDENTITY PRIMARY KEY,
+	[Name] VARCHAR(50) NOT NULL,
+	[ItemTypeID] INT FOREIGN KEY REFERENCES [ItemsTypes]([ItemTypeID]) NOT NULL
+)
+
+CREATE TABLE [Cities](
+	[CityID] INT IDENTITY PRIMARY KEY,
+	[Name] VARCHAR(50) UNIQUE NOT NULL
+)
+
+CREATE TABLE [Customers](
+	[CustomerID] INT IDENTITY PRIMARY KEY,
+	[Name] VARCHAR(50) NOT NULL,
+	[Birthday] DATE NOT NULL,
+	[CityID] INT FOREIGN KEY REFERENCES [Cities]([CityID])
+)
+
+CREATE TABLE [Orders](
+	[OrderID] INT IDENTITY PRIMARY KEY,
+	[CustomerID] INT FOREIGN KEY REFERENCES [Customers]([CustomerID]) NOT NULL
+)
+
+CREATE TABLE [OrderItems](
+	[OrderID] INT FOREIGN KEY REFERENCES [Orders]([OrderID]),
+	[ItemID] INT FOREIGN KEY REFERENCES [Items]([ItemID]),
+	PRIMARY KEY([OrderID], [ItemID])
+)
+
+
+
+
+
+DROP TABLE [ItemsTypes]
+DROP TABLE [OrderItems]
+DROP TABLE [Items]
+DROP TABLE [Cities]
+DROP TABLE [Customers]
+DROP TABLE [Orders]
