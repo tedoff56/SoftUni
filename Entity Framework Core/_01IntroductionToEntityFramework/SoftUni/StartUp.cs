01705ca5c8032ddcec3f744bc36686394a86c611
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-10-29 20:41:28 +0300

    up

---
+++ Entity Framework Core/_01IntroductionToEntityFramework/SoftUni/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using SoftUni.Data;
+
+namespace SoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext db = new SoftUniContext();
+
+            Console.WriteLine(GetEmployeesFullInformation(db));
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            var result = context.Employees
+                .Select(e => new {e.EmployeeId, e.FirstName, e.LastName, e.MiddleName, e.JobTitle, e.Salary})
+                .OrderBy(e => e.EmployeeId)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+            
+            result.ForEach(e => 
+                sb.AppendLine($"{e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.Salary:F2}"));
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
