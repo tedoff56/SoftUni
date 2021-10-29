6b1be094257398ebd705dbddda649570982a5e22
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2021-10-29 21:06:31 +0300

    UP

--- Entity Framework Core/_01IntroductionToEntityFramework/SoftUni/StartUp.cs
+++ Entity Framework Core/_01IntroductionToEntityFramework/SoftUni/StartUp.cs
@@ -5,13 +5,45 @@ using SoftUni.Data;
 
 namespace SoftUni
 {
-    class Program
+    public class StartUp
     {
         static void Main(string[] args)
         {
             SoftUniContext db = new SoftUniContext();
 
-            Console.WriteLine(GetEmployeesFullInformation(db));
+            Console.WriteLine(GetEmployeesFromResearchAndDevelopment(db));
+        }
+
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+        {
+            var result = context.Employees
+                .Select(e => new {e.FirstName, e.LastName, DepartmentName = e.Department.Name, e.Salary})
+                .Where(e => e.DepartmentName == "Research and Development")
+                .OrderBy(e => e.Salary)
+                .ThenByDescending(e => e.FirstName)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+            
+            result.ForEach(e => 
+                sb.AppendLine($"{e.FirstName} {e.LastName} from {e.DepartmentName} - ${e.Salary:F2}"));
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            var result = context.Employees
+                .Select(e => new {e.FirstName, e.Salary})
+                .Where(e => e.Salary > 50000)
+                .OrderBy(e => e.FirstName)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+            
+            result.ForEach(e => sb.AppendLine($"{e.FirstName} - {e.Salary:F2}"));
+
+            return sb.ToString().TrimEnd();
         }
 
         public static string GetEmployeesFullInformation(SoftUniContext context)
@@ -20,9 +52,9 @@ namespace SoftUni
                 .Select(e => new {e.EmployeeId, e.FirstName, e.LastName, e.MiddleName, e.JobTitle, e.Salary})
                 .OrderBy(e => e.EmployeeId)
                 .ToList();
-
-            StringBuilder sb = new StringBuilder();
             
+            StringBuilder sb = new StringBuilder();
+
             result.ForEach(e => 
                 sb.AppendLine($"{e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.Salary:F2}"));
 
