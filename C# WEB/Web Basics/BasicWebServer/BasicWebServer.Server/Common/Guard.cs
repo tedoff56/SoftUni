7146db2cd918c0b523c57eb23c83bf9b749244c9
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2022-01-25 10:12:28 +0200

    up

---
+++ C# WEB/Web Basics/BasicWebServer/BasicWebServer.Server/Common/Guard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BasicWebServer.Server.Common
+{
+    public static class Guard
+    {
+        public static void AgainstNull(object value, string name = null)
+        {
+            if (value is null)
+            {
+                name ??= "Value";
+                
+                throw new InvalidOperationException($"{name} cannot be null.");
+            }
+        }
+    }
+}
--- C# WEB/Web Basics/BasicWebServer/BasicWebServer.Server/HTTP/Header.cs
+++ C# WEB/Web Basics/BasicWebServer/BasicWebServer.Server/HTTP/Header.cs
@@ -1,9 +1,14 @@
-﻿namespace BasicWebServer.Server.HTTP
+﻿using BasicWebServer.Server.Common;
+
+namespace BasicWebServer.Server.HTTP
 {
     public class Header
     {
         public Header(string name, string value)
         {
+            Guard.AgainstNull(name, "Header name");
+            Guard.AgainstNull(value, "Header value");
+            
             this.Name = name;   
             this.Value = value;
         }
