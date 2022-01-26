ff1ac05ed5d76fb340e2ffaedef60eab74f7e1be
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2022-01-26 09:40:38 +0200

    Create the Form and Map Requests

--- C# WEB/Web Basics/BasicWebServer/BasicWebServer.Demo/StartUp.cs
+++ C# WEB/Web Basics/BasicWebServer/BasicWebServer.Demo/StartUp.cs
@@ -5,12 +5,21 @@ using BasicWebServer.Server.Responses;
 
 namespace BasicWebServer.Demo
 {
-    class Program
+    class StartUp
     {
+        private const string htmlFormString = @"
+            <form action='/HTML' method='POST'>
+                Name: <input type='text' name='Name'/>
+                Age: <input type='number' name='Age'/>
+                <input type='submit' value='Save'/>
+            </form>";
+            
+            
         static void Main(string[] args)
             => new HttpServer(r => r
                 .MapGet("/", new TextResponse("Hello from the server"))
-                .MapGet("/HTML", new HtmlResponse("<h1>HTML Response</h1>"))
+                .MapGet("/HTML", new HtmlResponse(htmlFormString))
+                .MapPost("/HTML", new TextResponse(""))
                 .MapGet("/Redirect", new RedirectResponse("https://abv.bg/")))
                 .Start();
     }
