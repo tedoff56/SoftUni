1cd7e6dc23592c9c79003241adc039d42cabd515
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2022-02-04 15:58:14 +0200

    1.	Implement Cookie Functionality

---
+++ C# WEB/Web Basics/BasicWebServer/BasicWebServer.Server/HTTP/CookieCollection.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace BasicWebServer.Server.HTTP
+{
+    public class CookieCollection : IEnumerable<Cookie>
+    {
+        private readonly Dictionary<string, Cookie> cookies;
+
+        public CookieCollection() 
+            => cookies = new Dictionary<string, Cookie>();
+        
+        private string this[string name]
+            => cookies[name].Value;
+        
+        private void Add(string name, string value) 
+            => cookies[name] = new Cookie(name, value);
+
+        private bool Contains(string name)
+            => cookies.ContainsKey(name);
+        
+        public IEnumerator<Cookie> GetEnumerator() 
+            => cookies.Values.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() 
+            => GetEnumerator();
+    }
+}
