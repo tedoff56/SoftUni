94e4b4ed14555f3e202b70229d6ba2cda5b4fac7
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2022-09-27 18:01:21 +0300

    Lowest Prices in Cities

---
+++ JS Advanced/_03 Objects & Composition/lowestPricesInCities.js
@@ -0,0 +1,33 @@
+function lowestPrice(input){
+    let result = [];
+
+    for (const townData of input) {
+        let [townName, productName, productPrice] = townData.split(' | ');
+
+        productPrice = Number(productPrice);
+
+        let town = result.find(r => r.productName === productName);
+
+        if(town === undefined){
+            result.push({productName, productPrice, townName})
+            continue;
+        }
+
+        if(productPrice < town.productPrice){
+            town.townName = townName;
+            town.productPrice = productPrice;
+        }
+
+    }
+
+    result.forEach(r => console.log(`${r.productName} -> ${r.productPrice} (${r.townName})`))
+}
+
+lowestPrice([
+    'Sample Town | Sample Product | 1000',
+    'Sample Town | Orange | 2',
+    'Sample Town | Peach | 1',
+    'Sofia | Orange | 3',
+    'Sofia | Peach | 2',
+    'New York | Sample Product | 1000.1',
+    'New York | Burger | 10']);
