a3e6680f1eaf67f8f829681b6acce66822fa7a38
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2022-10-01 10:25:42 +0300

    Pascal or Camel Case

--- JS Advanced/_04 DOM Introduction/02. Pascal or Camel Case/solution.js
+++ JS Advanced/_04 DOM Introduction/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,27 @@
 function solve() {
-  //TODO...
+  let textToTransform = document.getElementById('text').value;
+  let namingConvention = document.getElementById('naming-convention').value;
+  let result = document.getElementById('result');
+
+  let words = textToTransform.split(' ');
+  let resultString = '';
+
+  switch(namingConvention){
+    case 'Camel Case':
+      words.forEach((w, i) => {
+        if(i === 0){
+          return resultString += w[0].toLowerCase() + w.substring(1).toLowerCase();
+        }
+        return resultString += w[0].toUpperCase() + w.substring(1).toLowerCase();
+      })
+      break;
+      case 'Pascal Case':
+        words.forEach((w, i) => {
+          return resultString += w[0].toUpperCase() + w.substring(1).toLowerCase();
+        })
+        break;
+        default: resultString = 'Error!';
+  }
+
+  result.textContent = resultString;
 }
