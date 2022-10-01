cce55fa8ceee740d781778518475f4fab12c2d10
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2022-10-01 11:48:38 +0300

    Search in List

--- JS Advanced/_04 DOM Introduction/04. Search in List/search.js
+++ JS Advanced/_04 DOM Introduction/04. Search in List/search.js
@@ -1,3 +1,23 @@
 function search() {
-   // TODO
+   let towns = Array.from(document.querySelectorAll('#towns li'));
+   let searchText = document.getElementById('searchText').value;
+
+   let cnt = 0;
+   for (let town of towns) {
+      let townName = town.textContent;
+
+      if(townName.toLowerCase().includes(searchText.toLowerCase())){
+         cnt++;
+         town.style.fontWeight = 'bold';
+         town.style.textDecoration = 'underline'
+      }
+      else{
+         town.style.fontWeight = 'normal';
+         town.style.textDecoration = 'none';
+      }
+   }
+
+   document.getElementById('result').textContent = `${cnt} matches found`;
+
+   cnt = 0;
 }
