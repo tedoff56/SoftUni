c59427c5bff7a60aabd12931fc7e841e312d4f77
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2022-10-04 08:33:46 +0300

    Table – Search Engine

--- JS Advanced/_04 DOM Introduction/05. Table - Search Engine/solution.js
+++ JS Advanced/_04 DOM Introduction/05. Table - Search Engine/solution.js
@@ -2,7 +2,17 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      let rows = Array.from(document.querySelectorAll('tbody tr'));
+      let search = document.getElementById('searchField').value;
+
+      rows.forEach(d => {
+         d.classList.remove('select');
+
+         if(search !== '' && d.innerText.toLowerCase().includes(search.toLowerCase())){
+            d.className = 'select';
+         }
+      });
+      
 
    }
 }
