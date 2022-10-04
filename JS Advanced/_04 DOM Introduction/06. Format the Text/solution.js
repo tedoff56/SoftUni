2578972ceec2a8ff9b0249afe640035f18b2b6a3
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2022-10-04 09:03:56 +0300

    Format the Text

--- JS Advanced/_04 DOM Introduction/06. Format the Text/solution.js
+++ JS Advanced/_04 DOM Introduction/06. Format the Text/solution.js
@@ -1,3 +1,21 @@
 function solve() {
-  //TODO
+  let input = document.getElementById('input').value;
+  let output = document.getElementById('output');
+
+  
+  let sentences = input.split('.').filter(s => s.length > 0);
+
+
+  for (let i = 0; i < sentences.length; i += 3) {
+    let temp = '';
+
+    for (let x = 0; x < 3; x++){
+      if(sentences[i + x]){
+        temp += sentences[i + x].trim();
+      }
+    }
+
+    output.innerHTML += `<p>${temp}.</p>\n`;
+    temp = '';
+  }
 }
