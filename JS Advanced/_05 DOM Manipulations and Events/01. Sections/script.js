3d8146d3da055b5a8e71b56b1c68c739e19736aa
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2022-10-04 09:24:24 +0300

    Sections

--- JS Advanced/_05 DOM Manipulations and Events/01. Sections/script.js
+++ JS Advanced/_05 DOM Manipulations and Events/01. Sections/script.js
@@ -1,3 +1,18 @@
 function create(words) {
-   console.log('TODO:...');
+   
+   let content = document.getElementById('content');
+
+   for (let word of words) {
+      let div = document.createElement('DIV');
+      let p = document.createElement('P');
+      p.textContent = word;
+      p.style.display = 'none';
+
+      div.appendChild(p);
+      content.appendChild(div);
+
+      div.addEventListener('click', function(){
+         p.style.display = 'block';
+      })
+   }
 }
