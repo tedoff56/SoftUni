9a39dcc3828432fb045ee4a7b83ce3d604523f6f
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2022-10-04 11:34:36 +0300

    Encode and Decode Messages

--- JS Advanced/_05 DOM Manipulations and Events/05. Encode-and-Decode-Messages/app.js
+++ JS Advanced/_05 DOM Manipulations and Events/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,26 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+
+    let [encodeButton, decodeButton] = document.getElementsByTagName('button');
+    let [message, receivedMessage] = document.getElementsByTagName('textarea');
+
+    encodeButton.addEventListener('click', function() {
+        let encodedMessage = '';
+    
+        for (let char of message.value) {
+            encodedMessage += String.fromCharCode(char.charCodeAt(0) + 1)
+        }
+
+        message.value = '';
+        receivedMessage.value = encodedMessage;
+    });
+
+    decodeButton.addEventListener('click', function(){
+        let decodedMessage = '';
+
+        for(let char of receivedMessage.value){
+            decodedMessage += String.fromCharCode(char.charCodeAt(0) - 1);
+        }
+
+        receivedMessage.value = decodedMessage;
+    });
 }
