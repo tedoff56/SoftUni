77441322da3a58e6e5a737582bd3479f2fdf5be9
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2022-10-04 09:57:51 +0300

    Time Converter

--- JS Advanced/_05 DOM Manipulations and Events/02. Time-Converter/solution.js
+++ JS Advanced/_05 DOM Manipulations and Events/02. Time-Converter/solution.js
@@ -1,4 +1,40 @@
 function attachEventsListeners() {
 
-    console.log('TODO:...');
+    let buttons = Array.from(document.querySelectorAll('input[type=button]'))
+        .map(b => b.addEventListener('click', convert));
+
+
+    function convert(e){
+        let inputValue = e.target.parentElement.querySelector('input[type=text]').value;
+
+        switch(e.target.id){
+            case 'daysBtn':
+                populate(inputValue);
+            break;
+            case 'hoursBtn':
+                populate(inputValue / 24);
+            break;
+            case 'minutesBtn':
+                populate(inputValue / 24 / 60);
+            break;
+            case 'secondsBtn':
+                populate(inputValue / 24 / 60 / 60);
+            break;
+        }
+        console.log(input);
+    }
+
+    function populate(value){
+
+        let inputs = Array.from(document.querySelectorAll('input[type=text]'));
+        inputs.shift().value = value;
+
+        let currentValue = value * 24;
+
+        for(let input of inputs){
+            input.value = currentValue;
+            currentValue *= 60;
+        }
+
+    }
 }
