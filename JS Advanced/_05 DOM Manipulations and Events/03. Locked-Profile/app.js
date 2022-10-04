d2ff47c1f13d45b00d8e56c86270ca5a038b980a
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2022-10-04 10:26:38 +0300

    Locked Profile

--- JS Advanced/_05 DOM Manipulations and Events/03. Locked-Profile/app.js
+++ JS Advanced/_05 DOM Manipulations and Events/03. Locked-Profile/app.js
@@ -1,3 +1,30 @@
 function lockedProfile() {
-    console.log('TODO...')
+
+    let profiles = Array.from(document.querySelectorAll('.profile'));
+    
+    for(let profile of profiles){
+        profile.getElementsByTagName('button')[0].addEventListener('click', showMore);
+
+
+    }
+
+    function showMore(e){
+        let isLocked = e.target.parentElement.querySelectorAll('input[type=radio]')[0].checked === true;
+
+        if(isLocked){
+            return;
+        }
+
+        let hiddenFields = e.target.parentElement.getElementsByTagName('div')[0];
+
+        if(e.target.textContent == 'Hide it'){
+            hiddenFields.style.display = 'none';
+            e.target.textContent = 'Show more'
+        }
+        else{
+            hiddenFields.style.display = 'block';
+            e.target.textContent = 'Hide it';
+        }
+
+    }
 }
