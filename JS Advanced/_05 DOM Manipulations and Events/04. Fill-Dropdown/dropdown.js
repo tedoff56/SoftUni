65a0523cb34cf0528b6fb0a3bdb38eacce2b0d14
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2022-10-04 10:34:28 +0300

    Fill Dropdown

--- JS Advanced/_05 DOM Manipulations and Events/04. Fill-Dropdown/dropdown.js
+++ JS Advanced/_05 DOM Manipulations and Events/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,15 @@
 function addItem() {
-    console.log('TODO:...');
+    let text = document.getElementById('newItemText');
+    let value = document.getElementById('newItemValue');
+
+    let select = document.getElementById('menu');
+
+    let option = document.createElement('option');
+    option.textContent = text.value;
+    option.value = value.value;
+
+    select.appendChild(option);
+
+    text.value = '';
+    value.value = '';
 }
