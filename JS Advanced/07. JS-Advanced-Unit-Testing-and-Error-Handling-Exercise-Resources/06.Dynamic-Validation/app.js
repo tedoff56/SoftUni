58f4b87c6c29cfc94f117a7ac5a79641af9b46d0
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2022-10-10 19:14:51 +0300

    Dynamic Validation

--- JS Advanced/07. JS-Advanced-Unit-Testing-and-Error-Handling-Exercise-Resources/06.Dynamic-Validation/app.js
+++ JS Advanced/07. JS-Advanced-Unit-Testing-and-Error-Handling-Exercise-Resources/06.Dynamic-Validation/app.js
@@ -1,3 +1,14 @@
 function validate() {
-    // TODO
+    let email = document.getElementById('email');
+    let validRegex = /^[a-z]+\@[a-z]+\.[a-z]+$/;
+
+    email.addEventListener('change', () =>{
+        if(validRegex.test(email.value)){
+            email.classList.remove('error');
+            email.value = email.value.split('@')[0];
+        }
+        else{
+            email.classList.toggle('error')
+        }
+    })
 }
