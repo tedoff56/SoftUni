1026be9645db71d9c9fd273f7b1443612dccc1db
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2022-11-02 10:27:51 +0200

    Bus stop

--- JS Applications/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/01.Bus-Stop/app.js
+++ JS Applications/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/01.Bus-Stop/app.js
@@ -1,3 +1,26 @@
-function getInfo() {
-    console.log("TODO...");
+async function getInfo() {
+    let stopId = document.getElementById('stopId');
+    let buses = document.getElementById('buses');
+    let stopName = document.getElementById('stopName');
+
+    stopName.textContent = '';
+    buses.innerHTML = '';
+
+    try{
+        const url = `http://localhost:3030/jsonstore/bus/businfo/${stopId.value}`;
+        const response = await fetch(url);
+        let stopData = await response.json();
+    
+        buses.innerHTML = '';
+        stopName.textContent = stopData.name;
+    
+        for (const [busId, time] of Object.entries(stopData.buses)) {
+            let li = document.createElement('li');
+            li.textContent = `Bus ${busId} arrives in ${time} minutes`;
+            buses.appendChild(li);
+        }
+    }
+    catch(error){
+        stopName.textContent = 'Error';
+    }
 }
