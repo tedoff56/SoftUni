545688b019197e50f164470b880daca9b5f91eb6
tedoff56 <76625893+tedoff56@users.noreply.github.com>
2022-11-02 11:06:38 +0200

    Bus Schedule

--- JS Applications/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/02.Bus-Schedule/app.js
+++ JS Applications/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/02.Bus-Schedule/app.js
@@ -1,13 +1,34 @@
-function solve() {
+ function solve() {
+    let info = document.getElementsByClassName('info')[0];
+    let departBtn = document.getElementById('depart');
+    let arriveBtn = document.getElementById('arrive');
 
-    function depart() {
-        console.log('Depart TODO...');
-    }
+    let currentStopName = '';
+    let nextStopId = 'depot';
+
+    async function depart() {
+        try{
+            const response = await fetch(`http://localhost:3030/jsonstore/bus/schedule/${nextStopId}`)
+            const stop = await response.json();
+            info.textContent = `Next stop ${stop.name}`;
+            departBtn.disabled = true;
+            arriveBtn.disabled = false;
+            nextStopId = stop.next;
+            currentStopName = stop.name;
+        }
+        catch(error){
+            info.textContent = 'Error';
+            departBtn.disabled = true;
+            arriveBtn.disabled = true;
+        }
+    }   
 
     function arrive() {
-        console.log('Arrive TODO...');
+        info.textContent = `Arriving at ${currentStopName}`;
+        arriveBtn.disabled = true;
+        departBtn.disabled = false;
     }
-
+    
     return {
         depart,
         arrive
